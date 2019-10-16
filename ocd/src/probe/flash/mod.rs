4509bdbc03bc1d5e91155e0c0ef53c6ff662e3b2
Noah Hüsser <yatekii@yatekii.ch>
2019-10-16 23:17:15 +0200

    Add attribution notice

--- ocd/src/probe/flash/mod.rs
+++ ocd/src/probe/flash/mod.rs
@@ -1,3 +1,7 @@
+// Some parts of this file and subsequent module files might contain copyrighted code
+// which follows the logic of the [pyOCD debugger](https://github.com/mbedmicro/pyOCD) project.
+// Copyright (c) for that code 2015-2019 Arm Limited under the the Apache 2.0 license.
+
 pub mod builder;
 pub mod flasher;
 pub mod memory;
