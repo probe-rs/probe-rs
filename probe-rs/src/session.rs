ed738087a07bb78401ed0299cfe9d638b6a9d10f
Noah Hüsser <yatekii@yatekii.ch>
2020-02-06 02:00:52 +0100

    probe-rs should support autodetect again

--- probe-rs/src/session.rs
+++ probe-rs/src/session.rs
@@ -1,7 +1,9 @@
-use crate::architecture::arm::{memory::ADIMemoryInterface, ArmCommunicationInterface};
+use crate::architecture::arm::{ArmChipInfo, memory::ADIMemoryInterface, ArmCommunicationInterface};
 use crate::config::flash_algorithm::RawFlashAlgorithm;
 use crate::config::memory::MemoryRegion;
-use crate::config::target::Target;
+use crate::config::target::{TargetSpecification, Target};
+use crate::config::chip_info::ChipInfo;
+use crate::config::registry::{Registry, RegistryError};
 use crate::core::CoreType;
 use crate::{Core, CoreList, Error, Memory, MemoryList, Probe};
 use std::cell::RefCell;
@@ -23,17 +25,44 @@ enum ArchitectureSession {
 
 impl Session {
     /// Open a new session with a given debug target
-    pub fn new(probe: Probe, target: Target) -> Self {
+    pub fn new(probe: Probe, target: impl Into<TargetSpecification>) -> Result<Self, Error> {
         // TODO: Handle different architectures
 
-        let session = ArchitectureSession::Arm(ArmCommunicationInterface::new(probe));
+        let mut arm_interface = ArmCommunicationInterface::new(probe);
+        let registry = Registry::from_builtin_families();
 
-        Self {
+        let target = target.into();
+        let target = match target.into() {
+            TargetSpecification::Unspecified(name) => {
+                match registry.get_target_by_name(name) {
+                    Ok(target) => target,
+                    Err(err) => return Err(err)?,
+                }
+            },
+            TargetSpecification::Specified(target) => target,
+            TargetSpecification::ChipInfo => {
+                let arm_chip = ArmChipInfo::read_from_rom_table(&mut arm_interface)
+                    .map(|option| option.map(ChipInfo::Arm))?;
+                if let Some(chip) = arm_chip {
+                    match registry.get_target_by_chip_info(chip) {
+                        Ok(target) => target,
+                        Err(err) => return Err(err)?,
+                    }
+                } else {
+                    // Not sure if this is ok.
+                    return Err(Error::ChipNotFound(RegistryError::ChipAutodetectFailed))
+                }
+            }
+        };
+
+        let session = ArchitectureSession::Arm(arm_interface);
+
+        Ok(Self {
             inner: Rc::new(RefCell::new(InnerSession {
                 target,
                 architecture_session: session,
             })),
-        }
+        })
     }
 
     pub fn list_cores(&self) -> CoreList {
