b381bd9037099a7d02e43c94b8737af5a600df4d
Noah Hüsser <yatekii@yatekii.ch>
2020-02-07 22:10:22 +0100

    bump versions to 0.5.0 so we don't have conflicts with users

--- probe-rs/examples/ram_download.rs
+++ probe-rs/examples/ram_download.rs
@@ -1,4 +1,4 @@
-use probe_rs::{config::target::TargetSelector, Probe, WireProtocol};
+use probe_rs::{config::TargetSelector, Probe, WireProtocol};
 
 use std::num::ParseIntError;
 use std::time::Instant;
