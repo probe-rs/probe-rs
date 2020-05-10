b31bf6433948f97094bb1e5ce112df643dd559e7
Noah Hüsser <yatekii@yatekii.ch>
2020-05-10 17:01:35 +0200

    Add some docs

--- target-gen/src/fetch.rs
+++ target-gen/src/fetch.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
 use cmsis_pack::{pack_index::Vidx, utils::FromElem};
 
+/// Fetches the master VIDX/PIDX file from the ARM server and returns the parsed file.
 pub(crate) fn get_vidx() -> Result<Vidx> {
     let reader = reqwest::blocking::Client::new()
         .get("https://www.keil.com/pack/index.pidx")
