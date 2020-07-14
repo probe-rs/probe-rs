98b8ababa546f55f9655cc13125dd5059e5d5146
Henrik Böving <hargonix@gmail.com>
2020-07-14 14:53:02 +0200

    remove main, instead make probe-rs-server a lib with an exposed run function so other tooling can easily use it

--- probe-rs-server/src/lib.rs
+++ probe-rs-server/src/lib.rs
@@ -1,2 +1,18 @@
 pub mod definitions;
 pub mod server;
+
+use jsonrpc_core::IoHandler;
+use jsonrpc_ws_server::*;
+use crate::definitions::ProbeRsGenServer;
+use crate::server::Server;
+
+pub fn run(host: String, port: u16) {
+    let mut io = IoHandler::new();
+    let server = Server::new();
+    io.extend_with(server.to_delegate());
+    let server_addr = std::net::SocketAddr::new(host.parse().unwrap(), port);
+    let server = ServerBuilder::new(io)
+        .start(&server_addr)
+        .expect("Server must start with no issues");
+    server.wait().unwrap();
+}
