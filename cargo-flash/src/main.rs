2e22aa872acbb10ffd3dbbcad94d6fc00272483b
Noah Hüsser <yatekii@yatekii.ch>
2020-11-06 00:41:56 +0100

    Add generalized argument handling

--- cargo-flash/src/main.rs
+++ cargo-flash/src/main.rs
@@ -17,7 +17,7 @@ use probe_rs::{
     DebugProbeError, DebugProbeSelector, Probe, WireProtocol,
 };
 
-use probe_rs_cli_util::{build_artifact, logging, read_metadata};
+use probe_rs_cli_util::{argument_handling, build_artifact, logging, read_metadata};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -198,7 +198,7 @@ fn main_try() -> Result<()> {
     args.remove(0); // Remove executable name
 
     // Remove all arguments that `cargo build` does not understand.
-    remove_arguments(ARGUMENTS_TO_REMOVE, &mut args);
+    argument_handling::remove_arguments(ARGUMENTS_TO_REMOVE, &mut args);
 
     // Change the work dir if the user asked to do so
     std::env::set_current_dir(&work_dir).context("failed to change the working directory")?;
@@ -460,93 +460,6 @@ fn print_families() -> Result<()> {
         }
     }
     Ok(())
-}
-
-/// Removes all arguments from the commandline input that `cargo build` does not understand.
-/// All the arguments are removed in place!
-/// It expects a list of arguments to be removed. If the argument can have a value it MUST contain a `=` at the end.
-/// E.g:
-/// ```rust
-/// let arguments_to_remove = [
-///     "foo", // Can be "--foo"
-///     "bar=", // Can be "--bar=value" and "--bar value"
-/// ];
-/// ```
-fn remove_arguments(arguments_to_remove: &[&'static str], arguments: &mut Vec<String>) {
-    // We iterate all arguments that possibly have to be removed
-    // and remove them if they occur to be in the input.
-    for argument in arguments_to_remove {
-        // Make sure the compared against arg does not contain an equal sign.
-        // If the original arg contained an equal sign we take this as a hint
-        // that the arg can be used as `--arg value` as well as `--arg=value`.
-        // In the prior case we need to remove two arguments. So remember this.
-        let (remove_two, clean_argument) = if argument.ends_with('=') {
-            (true, format!("--{}", &argument[..argument.len() - 1]))
-        } else {
-            (false, format!("--{}", argument))
-        };
-
-        // Iterate all args in the input and if we find one that matches, we remove it.
-        if let Some(index) = arguments
-            .iter()
-            .position(|x| x.starts_with(&format!("--{}", argument)))
-        {
-            // We remove the argument we found.
-            arguments.remove(index);
-        }
-
-        // If the argument requires a value we also need to check for the case where no
-        // = (equal sign) was present, in which case the value is a second argument
-        // which we need to remove as well.
-        if remove_two {
-            // Iterate all args in the input and if we find one that matches, we remove it.
-            if let Some(index) = arguments
-                .iter()
-                .position(|x| x.starts_with(&clean_argument))
-            {
-                // We remove the argument we found plus its value.
-                arguments.remove(index);
-                arguments.remove(index);
-            }
-        }
-    }
-}
-
-#[test]
-fn remove_arguments_test() {
-    let arguments_to_remove = [
-        "chip=",
-        "chip-description-path=",
-        "list-chips",
-        "disable-progressbars",
-        "protocol=",
-        "probe-index=",
-        "reset-halt",
-        "nrf-recover",
-    ];
-
-    let mut arguments = vec![
-        "--chip=kek".to_string(),
-        "--chip".to_string(),
-        "kek".to_string(),
-        "--chip-description-path=kek".to_string(),
-        "--chip-description-path".to_string(),
-        "kek".to_string(),
-        "--list-chips".to_string(),
-        "--disable-progressbars".to_string(),
-        "--protocol=kek".to_string(),
-        "--protocol".to_string(),
-        "kek".to_string(),
-        "--probe-index=kek".to_string(),
-        "--probe-index".to_string(),
-        "kek".to_string(),
-        "--reset-halt".to_string(),
-        "--nrf-recover".to_string(),
-    ];
-
-    remove_arguments(&arguments_to_remove, &mut arguments);
-
-    assert!(arguments.is_empty());
 }
 
 /// Lists all connected devices
