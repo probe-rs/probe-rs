682d21b9413fed23c20679a574815bc02340a2fc
Jonas Schievink <jonasschievink@gmail.com>
2021-05-27 15:49:48 +0200

    Include address in missing debuginfo error

--- src/backtrace/unwind.rs
+++ src/backtrace/unwind.rs
@@ -13,10 +13,12 @@ use crate::{
     Outcome, VectorTable,
 };
 
-static MISSING_DEBUG_INFO: &str = "debug information is missing. Likely fixes:
-1. compile the Rust code with `debug = 1` or higher. This is configured in the `profile.{release,bench}` sections of Cargo.toml (`profile.{dev,test}` default to `debug = 2`)
-2. use a recent version of the `cortex-m` crates (e.g. cortex-m 0.6.3 or newer). Check versions in Cargo.lock
-3. if linking to C code, compile the C code with the `-g` flag";
+fn missing_debug_info(pc: u32) -> String {
+    format!("debug information for address {:#x} is missing. Likely fixes:
+        1. compile the Rust code with `debug = 1` or higher. This is configured in the `profile.{{release,bench}}` sections of Cargo.toml (`profile.{{dev,test}}` default to `debug = 2`)
+        2. use a recent version of the `cortex-m` crates (e.g. cortex-m 0.6.3 or newer). Check versions in Cargo.lock
+        3. if linking to C code, compile the C code with the `-g` flag", pc)
+}
 
 /// Virtually* unwinds the target's program
 /// \* destructors are not run
@@ -65,7 +67,7 @@ pub(crate) fn target(
                 pc.into(),
                 DebugFrame::cie_from_offset,
             )
-            .with_context(|| MISSING_DEBUG_INFO)?;
+            .with_context(|| missing_debug_info(pc))?;
 
         let cfa_changed = registers.update_cfa(uwt_row.cfa())?;
 
