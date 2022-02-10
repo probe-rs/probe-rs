9851d104e3b48795a2c3a30464077285c225f776
Johann Hemmann <johann.hemmann@code.berlin>
2022-02-10 16:06:08 +0100

    Revert "Only check if start of stack is currently less than the highest address of the section"
    This reverts commit fcb2b90587a3229547c7c41c98f4c670c76d445c.

--- src/target_info.rs
+++ src/target_info.rs
@@ -97,7 +97,7 @@ fn extract_stack_info(elf: &Elf, ram_range: &Range<u32>) -> Option<StackInfo> {
                     name
                 );
                 return None;
-            } else if stack_start <= *section_range.end() {
+            } else if section_range.contains(&stack_start) {
                 stack_start = section_range.end() + 1;
             }
         }
