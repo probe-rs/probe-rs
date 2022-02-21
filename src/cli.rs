99c31a89856c1f5487724ed8f30cd0319d4f0028
Hidekazu Kobayashi <kobahide789@gmail.com>
2022-02-21 12:53:10 +0900

    Add way to pass chip description file

--- src/cli.rs
+++ src/cli.rs
@@ -27,6 +27,9 @@ pub(crate) struct Opts {
     #[structopt(long, required_unless_one(&["list-chips", "list-probes", "version"]), env = "PROBE_RUN_CHIP")]
     chip: Option<String>,
 
+    #[structopt(name = "chip description file path", long = "chip-description-path")]
+    pub(crate) chip_description_path: Option<PathBuf>,
+
     /// The probe to use (eg. `VID:PID`, `VID:PID:Serial`, or just `Serial`).
     #[structopt(long, env = "PROBE_RUN_PROBE")]
     pub(crate) probe: Option<String>,
--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,9 @@ fn run_target_program(elf_path: &Path, chip_name: &str, opts: &cli::Opts) -> any
     let elf_bytes = fs::read(elf_path)?;
     let elf = &Elf::parse(&elf_bytes)?;
 
+    if let Some(ref cdp) = opts.chip_description_path {
+        probe_rs::config::add_target_from_yaml(Path::new(cdp))?;
+    }
     let target_info = TargetInfo::new(chip_name, elf)?;
 
     let probe = probe::open(opts)?;
