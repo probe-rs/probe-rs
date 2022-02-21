e21a6c36587e0644ca8cc49dea723eac937e4b9f
Johann Hemmann <johann.hemmann@code.berlin>
2022-02-21 14:28:55 +0100

    Make stack-painting faster!

--- src/canary.rs
+++ src/canary.rs
@@ -1,8 +1,8 @@
 use std::time::Instant;
 
-use crate::TIMEOUT;
-use crate::{Elf, TargetInfo};
-use probe_rs::{MemoryInterface, Session};
+use probe_rs::{Core, MemoryInterface, Session};
+
+use crate::{registers::PC, Elf, TargetInfo, TIMEOUT};
 
 const CANARY_VALUE: u8 = 0xAA;
 
@@ -45,6 +45,7 @@ pub(crate) struct Canary {
 }
 
 impl Canary {
+    /// Decide if and where to place the stack canary.
     pub(crate) fn install(
         sess: &mut Session,
         target_info: &TargetInfo,
@@ -53,8 +54,6 @@ impl Canary {
     ) -> Result<Option<Self>, anyhow::Error> {
         let mut core = sess.core(0)?;
         core.reset_and_halt(TIMEOUT)?;
-
-        // Decide if and where to place the stack canary.
 
         let stack_info = match &target_info.stack_info {
             Some(range) => range,
@@ -69,7 +68,9 @@ impl Canary {
             return Ok(None);
         }
 
-        let stack_available = stack_info.range.end() - stack_info.range.start() - 1;
+        let stack_start = *stack_info.range.start();
+        let stack_end = *stack_info.range.end() - 1;
+        let stack_available = stack_end - stack_start;
 
         let size = if measure_stack {
             // When measuring stack consumption, we have to color the whole stack.
@@ -97,10 +98,8 @@ impl Canary {
                 size_kb
             );
         }
-        let address = *stack_info.range.start();
-        let canary = vec![CANARY_VALUE; size];
         let start = Instant::now();
-        core.write_8(address, &canary)?;
+        paint_stack(&mut core, stack_start, stack_start + size as u32)?;
         let seconds = start.elapsed().as_secs_f64();
         log::trace!(
             "setting up canary took {:.3}s ({:.2} KiB/s)",
@@ -109,7 +108,7 @@ impl Canary {
         );
 
         Ok(Some(Canary {
-            address,
+            address: stack_start,
             size,
             stack_available,
             data_below_stack: stack_info.data_below_stack,
@@ -185,4 +184,100 @@ impl Canary {
             }
         }
     }
+}
+
+/// Write [`CANARY_VALUE`] to the stack.
+fn paint_stack(core: &mut Core, start: u32, mut end: u32) -> Result<(), probe_rs::Error> {
+    // make sure stack_end is properly aligned
+    if end % 4 != 0 {
+        end += end % 4;
+    }
+
+    // construct subroutine
+    let subroutine = subroutine(start + SUBROUTINE_LENGTH, end);
+
+    // does the subroutine fit inside the stack?
+    let stack_size = (end - start) as usize;
+    assert!(
+        subroutine.len() < stack_size,
+        "subroutine doesn't fit inside stack"
+    );
+
+    // write subroutine to RAM
+    core.write_8(start, &subroutine)?;
+
+    // store current PC and set PC to beginning of subroutine
+    let previous_pc = core.read_core_reg(PC)?;
+    core.write_core_reg(PC, start)?;
+
+    // execute the subroutine and wait for it to finish
+    core.run()?;
+    core.wait_for_core_halted(TIMEOUT).unwrap();
+
+    // overwrite subroutine
+    core.write_8(start, &vec![CANARY_VALUE; subroutine.len()])?;
+
+    // reset PC to where it was before
+    core.write_core_reg(PC, previous_pc)?;
+
+    Ok(())
+}
+
+/// The length of the subroutine.
+///
+/// This is checked on runtime and needs to be updated when changing the subroutine.
+const SUBROUTINE_LENGTH: u32 = 28;
+
+/// Create a subroutine to paint [`CANARY_VALUE`] from `start` till `end`.
+///
+/// Both `start` and `end` need to be 4-byte-aligned.
+//
+// Roughly corresponds to following assembly:
+//
+// 00000108 <start>:
+//  108:   4803        ldr r0, [pc, #12]   ; (118 <end+0x2>)
+//  10a:   4904        ldr r1, [pc, #16]   ; (11c <end+0x6>)
+//  10c:   4a04        ldr r2, [pc, #16]   ; (120 <end+0xa>)
+//
+// 0000010e <loop>:
+//  10e:   4281        cmp r1, r0
+//  110:   d001        beq.n   116 <end>
+//  112:   c004        stmia   r0!, {r2}
+//  114:   e7fb        b.n 10e <loop>
+//
+// 00000116 <end>:
+//  116:   be00        bkpt    0x0000
+//  118:   20000100    .word   0x20000100  ; start
+//  11c:   20000200    .word   0x20000200  ; end
+//  120:   aaaaaaaa    .word   0xaaaaaaaa  ; pattern
+fn subroutine(start: u32, end: u32) -> Vec<u8> {
+    assert!(start < end, "start needs to be smaller than end address");
+    assert_eq!(start % 4, 0, "`start` needs to be 4-byte-aligned");
+    assert_eq!(end % 4, 0, "`end` needs to be 4-byte-aligned");
+
+    // convert start and end address to bytes
+    let start_bytes = start.to_le_bytes();
+    let end_bytes = end.to_le_bytes();
+
+    let mut subroutine = vec![
+        0x03, 0x48, // ldr
+        0x04, 0x49, // ldr
+        0x04, 0x4a, // ldr
+        0x81, 0x42, // cmp
+        0x01, 0xD0, // beq.n
+        0x04, 0xC0, // stmia
+        0xFB, 0xE7, // b.n
+        0x00, 0xBE, // bkpt
+    ];
+
+    // add the start and end address
+    subroutine.extend(start_bytes);
+    subroutine.extend(end_bytes);
+
+    // add the pattern to be painted
+    subroutine.extend([CANARY_VALUE; 4]);
+
+    assert_eq!(subroutine.len() as u32, SUBROUTINE_LENGTH);
+
+    subroutine
 }
