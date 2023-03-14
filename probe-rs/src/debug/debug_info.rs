39a92e29406849a7a7cffeff951a56d010a766ab
JackN <noppej@hotmail.com>
2023-03-14 08:55:57 -0400

    Add doc clarification.

--- probe-rs/src/debug/debug_info.rs
+++ probe-rs/src/debug/debug_info.rs
@@ -1126,6 +1126,7 @@ impl DebugInfo {
 
 /// Uses the [std::fs::canonicalize] function to canonicalize both paths before applying the [std::path::PathBuf::eq]
 /// to test if the secondary path is equal or a suffix of the primary path.
+/// If for some reason (e.g., the paths don't exist) the canonicalization fails, the original equality check is used.
 /// We do this to maximize the chances of finding a match where the secondary path can be given as
 /// an absolute, relative, or partial path.
 pub(crate) fn canonical_path_eq(primary_path: &Path, secondary_path: &Path) -> bool {
