b4813c1bb85fd392cf02b2ed922220b7bdc9abba
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-06-13 03:00:08 +0200

    Make cargo embed/flash shims build on Windows.

--- probe-rs/src/bin/cargo-embed.rs
+++ probe-rs/src/bin/cargo-embed.rs
@@ -1,10 +1,24 @@
+#[cfg(unix)]
 use std::os::unix::process::CommandExt;
-use std::process::Command;
+use std::process::{exit, Command};
 
 fn main() {
     let mut args: Vec<_> = std::env::args_os().skip(1).collect();
     args.insert(0, "cargo-embed".into());
-    let err = Command::new("probe-rs").args(&args).exec();
+    let mut cmd = Command::new("probe-rs");
+    cmd.args(&args);
+
+    #[cfg(unix)]
+    let err = cmd.exec();
+    #[cfg(not(unix))]
+    let err = match cmd.spawn() {
+        Ok(mut child) => match child.wait() {
+            Ok(exitcode) => exit(exitcode.code().unwrap_or(98)),
+            Err(e) => e,
+        },
+        Err(e) => e,
+    };
+
     eprintln!("Error: {}", err);
-    std::process::exit(99);
+    exit(99);
 }
