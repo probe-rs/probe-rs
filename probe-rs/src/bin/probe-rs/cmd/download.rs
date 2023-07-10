probe-rs/probe-rs

commit c0610e98008cbb34d0dc056fcddff0f2d4f50ad5

Move each subcommand to its own mod, cleanups. (#1649)

probe-rs/src/bin/probe-rs/cmd/download.rs
@@ -1,0 +1,105 @@
+use std::fs::File;
+use std::path::Path;
+
+use anyhow::Context;
+use probe_rs::flashing::FileDownloadError;
+use probe_rs::flashing::{BinOptions, Format};
+
+use crate::util::common_options::ProbeOptions;
+use crate::util::common_options::{CargoOptions, FlashOptions};
+use crate::util::flash::run_flash_download;
+use crate::util::parse_u32;
+use crate::util::parse_u64;
+
+#[derive(clap::ValueEnum, Debug, Clone, Copy)]
+enum DownloadFileType {
+    Elf,
+    Hex,
+    Bin,
+}
+
+impl DownloadFileType {
+    fn into(self, base_address: Option<u64>, skip: Option<u32>) -> Format {
+        match self {
+            DownloadFileType::Elf => Format::Elf,
+            DownloadFileType::Hex => Format::Hex,
+            DownloadFileType::Bin => Format::Bin(BinOptions {
+                base_address,
+                skip: skip.unwrap_or(0),
+            }),
+        }
+    }
+}
+
+#[derive(clap::Parser)]
+pub struct Cmd {
+    #[clap(flatten)]
+    common: ProbeOptions,
+
+    /// Format of the file to be downloaded to the flash. Possible values are case-insensitive.
+    #[clap(value_enum, ignore_case = true, default_value = "elf", long)]
+    format: DownloadFileType,
+
+    /// The address in memory where the binary will be put at. This is only considered when `bin` is selected as the format.
+    #[clap(long, value_parser = parse_u64)]
+    base_address: Option<u64>,
+    /// The number of bytes to skip at the start of the binary file. This is only considered when `bin` is selected as the format.
+    #[clap(long, value_parser = parse_u32)]
+    skip_bytes: Option<u32>,
+
+    /// The path to the file to be downloaded to the flash
+    path: String,
+
+    /// Whether to erase the entire chip before downloading
+    #[clap(long)]
+    chip_erase: bool,
+
+    /// Whether to disable fancy progress reporting
+    #[clap(long)]
+    disable_progressbars: bool,
+
+    /// Disable double-buffering when downloading flash.  If downloading times out, try this option.
+    #[clap(long = "disable-double-buffering")]
+    disable_double_buffering: bool,
+}
+
+impl Cmd {
+    pub fn run(self) -> anyhow::Result<()> {
+        let mut session = self.common.simple_attach()?;
+
+        let mut file = match File::open(&self.path) {
+            Ok(file) => file,
+            Err(e) => return Err(FileDownloadError::IO(e)).context("Failed to open binary file."),
+        };
+
+        let mut loader = session.target().flash_loader();
+
+        let format = self.format.into(self.base_address, self.skip_bytes);
+        match format {
+            Format::Bin(options) => loader.load_bin_data(&mut file, options),
+            Format::Elf => loader.load_elf_data(&mut file),
+            Format::Hex => loader.load_hex_data(&mut file),
+        }?;
+
+        run_flash_download(
+            &mut session,
+            Path::new(&self.path),
+            &FlashOptions {
+                disable_progressbars: self.disable_progressbars,
+                disable_double_buffering: self.disable_double_buffering,
+                reset_halt: false,
+                log: None,
+                restore_unwritten: false,
+                flash_layout_output_path: None,
+                elf: None,
+                work_dir: None,
+                cargo_options: CargoOptions::default(),
+                probe_options: self.common,
+            },
+            loader,
+            self.chip_erase,
+        )?;
+
+        Ok(())
+    }
+}

commit 1fd7aaedc534f9e995cee1a691a026678048c363

fix: download cmd `--base-address` collision
In debug mode this would cause a panic due to duplicate arg.
In release, it would effectively silently ignore `--base-address`.
Regression happened in PR #1649.

fix: download cmd removed obsolete `--skip-bytes`

Literally did nothing, correct arg is `--skip`.
Regression happened in #1649.

fix: download cmd `--skip` now supports hexadecimal

Obsolete `--skip-bytes` would accept hexadecimal, but had no effect.
Regression happened in PR #1649.

probe-rs/src/bin/probe-rs/cmd/download.rs
@@ -8,21 +8,12 @@ use probe_rs::flashing::Format;
 use crate::util::common_options::ProbeOptions;
 use crate::util::common_options::{CargoOptions, FlashOptions};
 use crate::util::flash::run_flash_download;
-use crate::util::parse_u32;
-use crate::util::parse_u64;
 use crate::FormatOptions;
 
 #[derive(clap::Parser)]
 pub struct Cmd {
     #[clap(flatten)]
     common: ProbeOptions,
-
-    /// The address in memory where the binary will be put at. This is only considered when `bin` is selected as the format.
-    #[clap(long, value_parser = parse_u64)]
-    base_address: Option<u64>,
-    /// The number of bytes to skip at the start of the binary file. This is only considered when `bin` is selected as the format.
-    #[clap(long, value_parser = parse_u32)]
-    skip_bytes: Option<u32>,
 
     /// The path to the file to be downloaded to the flash
     path: String,

probe-rs/src/bin/probe-rs/main.rs
@@ -19,6 +19,9 @@ use tracing_subscriber::{
     EnvFilter, Layer,
 };
 
+use crate::util::parse_u32;
+use crate::util::parse_u64;
+
 #[derive(clap::Parser)]
 #[clap(
     name = "probe-rs",
@@ -93,11 +96,11 @@ pub(crate) struct FormatOptions {
     #[clap(value_enum, ignore_case = true, default_value = "elf", long)]
     #[serde(deserialize_with = "format_from_str")]
     format: Format,
-    /// The address in memory where the binary will be put at.
-    #[clap(long)]
+    /// The address in memory where the binary will be put at. This is only considered when `bin` is selected as the format.
+    #[clap(long, value_parser = parse_u64)]
     pub base_address: Option<u64>,
-    /// The number of bytes to skip at the start of the binary file.
-    #[clap(long, default_value = "0")]
+    /// The number of bytes to skip at the start of the binary file. This is only considered when `bin` is selected as the format.
+    #[clap(long, value_parser = parse_u32, default_value = "0")]
     pub skip: u32,
     /// The idf bootloader path
     #[clap(long)]