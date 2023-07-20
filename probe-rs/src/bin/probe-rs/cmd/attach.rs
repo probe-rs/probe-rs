d3580ed9ce1dd3766b081f8ff04c0225cf38b2ac
Roma Sokolov <sokolov.r.v@gmail.com>
2023-07-20 22:50:37 +0200

    Do not reset core when attaching

--- probe-rs/src/bin/probe-rs/cmd/attach.rs
+++ probe-rs/src/bin/probe-rs/cmd/attach.rs
@@ -26,36 +26,32 @@ impl Cmd {
         let memory_map = session.target().memory_map.clone();
 
         let mut core = session.core(0)?;
-        core.reset()?;
 
-        let mut rtta = match rtt::attach_to_rtt(
+        match rtt::attach_to_rtt(
             &mut core,
             &memory_map,
             Path::new(&self.path),
             &rtt_config,
             timestamp_offset,
         ) {
-            Ok(target_rtt) => Some(target_rtt),
+            Ok(mut rtta) => {
+                let mut stdout = std::io::stdout();
+                loop {
+                    for (_ch, data) in rtta.poll_rtt_fallible(&mut core)? {
+                        stdout.write_all(data.as_bytes())?;
+                    }
+
+                    // Poll RTT with a frequency of 10 Hz
+                    //
+                    // If the polling frequency is too high,
+                    // the USB connection to the probe can become unstable.
+                    std::thread::sleep(Duration::from_millis(100));
+                }
+            },
             Err(error) => {
-                log::error!("{:?} Continuing without RTT... ", error);
-                None
+                log::error!("{:?} RTT is not available.", error);
             }
         };
-
-        if let Some(rtta) = &mut rtta {
-            let mut stdout = std::io::stdout();
-            loop {
-                for (_ch, data) in rtta.poll_rtt_fallible(&mut core)? {
-                    stdout.write_all(data.as_bytes())?;
-                }
-
-                // Poll RTT with a frequency of 10 Hz
-                //
-                // If the polling frequency is too high,
-                // the USB connection to the probe can become unstable.
-                std::thread::sleep(Duration::from_millis(100));
-            }
-        }
 
         Ok(())
     }
