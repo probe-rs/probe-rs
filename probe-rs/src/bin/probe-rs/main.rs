d64af962c63648ad0faf85543eaf446f3db6df14
Dominik Boehi <dboehi@fastmail.com>
2023-09-22 16:09:46 +0200

    Remove dump subcommand, cleanup help output

--- probe-rs/src/bin/probe-rs/main.rs
+++ probe-rs/src/bin/probe-rs/main.rs
@@ -53,8 +53,6 @@ enum Subcommand {
     Gdb(cmd::gdb::Cmd),
     /// Basic command line debugger
     Debug(cmd::debug::Cmd),
-    /// Dump memory from attached target
-    Dump(cmd::dump::Cmd),
     /// Download memory to attached target
     Download(cmd::download::Cmd),
     /// Erase all nonvolatile memory of attached target
@@ -74,24 +72,7 @@ enum Subcommand {
     Chip(cmd::chip::Cmd),
     Benchmark(cmd::benchmark::Cmd),
     Profile(cmd::profile::Cmd),
-    /// Read from target memory address
-    /// e.g. probe-rs read b32 0x400E1490 2
-    ///      Reads 2 32-bit words from address 0x400E1490
-    /// Output is a space separated list of hex values padded to the read word width.
-    /// e.g. 2 words
-    ///     00 00 (8-bit)
-    ///     00000000 00000000 (32-bit)
-    ///     0000000000000000 0000000000000000 (64-bit)
-    ///
-    /// NOTE: Only supports RAM addresses
-    #[clap(verbatim_doc_comment)]
     Read(cmd::read::Cmd),
-    /// Write to target memory address
-    /// e.g. probe-rs write b32 0x400E1490 0xDEADBEEF 0xCAFEF00D
-    ///      Writes 0xDEADBEEF to address 0x400E1490 and 0xCAFEF00D to address 0x400E1494
-    ///
-    /// NOTE: Only supports RAM addresses
-    #[clap(verbatim_doc_comment)]
     Write(cmd::write::Cmd),
 }
 
@@ -271,7 +252,6 @@ fn main() -> Result<()> {
         Subcommand::Gdb(cmd) => cmd.run(),
         Subcommand::Reset(cmd) => cmd.run(),
         Subcommand::Debug(cmd) => cmd.run(),
-        Subcommand::Dump(cmd) => cmd.run(),
         Subcommand::Download(cmd) => cmd.run(),
         Subcommand::Run(cmd) => cmd.run(true, utc_offset),
         Subcommand::Attach(cmd) => cmd.run(utc_offset),
