8532e70467def20cf54efc987c73b8f9d98cb765
Dominik Boehi <dboehi@fastmail.com>
2023-09-24 05:47:43 +0200

    Remove dump subcommand, cleanup help output (#1772)

--- probe-rs/src/bin/probe-rs/cmd/read.rs
+++ probe-rs/src/bin/probe-rs/cmd/read.rs
@@ -3,7 +3,20 @@ use probe_rs::MemoryInterface;
 use crate::util::common_options::{ProbeOptions, ReadWriteBitWidth, ReadWriteOptions};
 use crate::CoreOptions;
 
+/// Read from target memory address
+///
+/// e.g. probe-rs read b32 0x400E1490 2
+///      Reads 2 32-bit words from address 0x400E1490
+///
+/// Output is a space separated list of hex values padded to the read word width.
+/// e.g. 2 words
+///     00 00 (8-bit)
+///     00000000 00000000 (32-bit)
+///     0000000000000000 0000000000000000 (64-bit)
+///
+/// NOTE: Only supports RAM addresses
 #[derive(clap::Parser)]
+#[clap(verbatim_doc_comment)]
 pub struct Cmd {
     #[clap(flatten)]
     shared: CoreOptions,
