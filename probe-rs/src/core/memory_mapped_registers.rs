probe-rs/probe-rs

commit 35e1afc0199e99337858e3fdb86dafdd4dfa12ca

Code refactoring related to handling of memory mapped registers (#1604)
Co-authored-by: Dominik Boehi <dboehi@fastmail.com>

probe-rs/src/core/memory_mapped_registers.rs
@@ -1,0 +1,95 @@
+/// A memory mapped register, for instance ARM debug registers (DHCSR, etc).
+pub trait MemoryMappedRegister<T>: Clone + From<T> + Into<T> + Sized + std::fmt::Debug {
+    /// The register's address in the target memory.
+    /// For some architectures (e.g. ARM Cortex-A) this address is offset from a base address.
+    /// For others (e.g. ARM Cortex-M, RISC-V) this address is absolute.
+    const ADDRESS_OFFSET: u64;
+    /// The register's name.
+    const NAME: &'static str;
+    /// Get the register's address in the memory map.
+    /// For architectures like ARM Cortex-A, this address is offset from a base address, which must be supplied when calling this method.
+    /// For others (e.g. ARM Cortex-M, RISC-V) where this address is a constant, please use [`MemoryMappedRegister::get_mmio_address`].
+    fn get_mmio_address_from_base(base_address: u64) -> Result<u64, anyhow::Error> {
+        if let Some(mmio_address) = base_address.checked_add(Self::ADDRESS_OFFSET) {
+            Ok(mmio_address)
+        } else {
+            Err(anyhow::anyhow!("Overflow while attempting to determine the MMIO address for register {} at offset {:#x} from base address {:#x}", Self::NAME, Self::ADDRESS_OFFSET, base_address))
+        }
+    }
+    /// Get the register's address in the memory map.
+    /// For architectures ARM Cortex-M and RISC-V, this address is constant value stored as part of [`MemoryMappedRegister::ADDRESS_OFFSET`].
+    /// For other architectures (e.g. ARM Cortex-A) where this address is offset from a base address, please use [`MemoryMappedRegister::get_mmio_address_from_base`].
+    fn get_mmio_address() -> u64 {
+        Self::ADDRESS_OFFSET
+    }
+}
+
+#[macro_export]
+/// Create a [`MemoryMappedRegister`] type, with the required method implementations for:
+/// - Trait implementations required by [`MemoryMappedRegister`]
+/// - Includes a `bitfield!` mapping for bitfield access to optionally defined fields.
+/// When no bitfields are defined, the default `.0` field must be used.
+///
+///! # Example
+/// ```
+/// use bitfield::bitfield;
+/// use probe_rs::memory_mapped_bitfield_register;
+/// memory_mapped_bitfield_register! {
+///    /// Abstract Control and Status (see section xyz of some reference manual)
+///    pub struct Abstractcs(u32);
+///    0x16,"abstractcs",
+///    impl From;
+///    progbufsize, _: 28, 24;
+///    busy, _: 12;
+///    cmderr, set_cmderr: 10, 8;
+///    datacount, _: 3, 0;
+/// }
+/// ```
+/// This will generate a struct with the name `Abstractcs`, which has:
+/// - A `pub` visibility.
+/// - A `u32` register type.
+/// - A `Debug` implementation.
+/// - A `Copy` implementation.
+/// - A `Clone` implementation.
+/// - Default `From<u32>` and `From<MemoryMappedRegister>` impls for [`MemoryMappedRegister`] are generated.
+/// - A `bitfield!` mapping for the fields `progbufsize`, `busy`, `cmderr`, `datacount`.
+/// - `bitfield!` getters and setters for the fields as defined - See [`bitfield::bitfield!`] for more information.
+/// - A `const ADDRESS: u64 = 0x16;`.
+/// - A `const NAME: &'static str = "abstractcs";`.
+macro_rules! memory_mapped_bitfield_register {
+    ($(#[$outer:meta])* $visibility:vis struct $struct_name:ident($reg_type:ty); $addr:expr, $reg_name:expr, impl From; $($rest:tt)*) => {
+        $crate::memory_mapped_bitfield_register!{
+            $(#[$outer])* $visibility struct $struct_name($reg_type); $addr, $reg_name, $($rest)*
+        }
+
+        impl From<$struct_name> for $reg_type {
+            fn from(register: $struct_name) -> Self {
+                register.0
+            }
+        }
+
+        impl From<$reg_type> for $struct_name {
+            fn from(value: $reg_type) -> Self {
+                Self(value)
+            }
+        }
+    };
+    ($(#[$outer:meta])* $vis_modifier:vis struct $struct_name:ident($reg_type:ty); $addr:expr, $reg_name:expr, $($rest:tt)*) => {
+        // Using paste here, because as of bitfield = "0.14.0" they do not use the 'vis' specifier, and balks at being passed a visibility token.
+        paste::paste!{
+        bitfield::bitfield!{
+            $(#[$outer])*
+            #[doc= concat!("A [`bitfield::bitfield!`] register mapping for the register `",  $reg_name, "` located at address `", stringify!($addr), "`.")]
+            #[derive(Copy, Clone)]
+            $vis_modifier struct $struct_name($reg_type);
+            impl Debug;
+            $($rest)*
+        }
+    }
+
+        impl $crate::MemoryMappedRegister<$reg_type> for $struct_name {
+            const ADDRESS_OFFSET: u64 = $addr;
+            const NAME: &'static str = $reg_name;
+        }
+    };
+}

cargo-embed/Cargo.toml
@@ -20,9 +20,7 @@ sentry = ["probe-rs-cli-util/sentry"]
 [dependencies]
 probe-rs = { workspace = true }
 gdb-server = { workspace = true }
-probe-rs-cli-util = { workspace = true, default-features = false, features = [
-    "anyhow",
-] }
+probe-rs-cli-util = { workspace = true, features = ["anyhow"] }
 git-version = "0.3.5"
 env_logger = "0.10.0"
 log = { version = "0.4.17", features = ["serde"] }

cargo-flash/Cargo.toml
@@ -24,9 +24,7 @@ sentry = ["probe-rs-cli-util/sentry"]
 env_logger = "0.10"
 colored = "2"
 probe-rs = { workspace = true }
-probe-rs-cli-util = { workspace = true, default-features = false, features = [
-    "anyhow",
-] }
+probe-rs-cli-util = { workspace = true, features = ["anyhow"] }
 anyhow = "1"
 bytesize = "1"
 thiserror = "1"

cli/src/info.rs
@@ -253,9 +253,9 @@ fn handle_memory_ap(
 ) -> Result<Tree<String>, anyhow::Error> {
     let component = {
         let mut memory = interface.memory_interface(access_port)?;
-        let mut demcr = Demcr(memory.read_word_32(Demcr::ADDRESS)?);
+        let mut demcr = Demcr(memory.read_word_32(Demcr::get_mmio_address())?);
         demcr.set_dwtena(true);
-        memory.write_word_32(Demcr::ADDRESS, demcr.into())?;
+        memory.write_word_32(Demcr::get_mmio_address(), demcr.into())?;
         Component::try_parse(&mut *memory, base_address)?
     };
     let component_tree = coresight_component_tree(interface, component, access_port)?;

probe-rs/Cargo.toml
@@ -54,6 +54,7 @@ object = { version = "0.31.1", default-features = false, features = [
     "read_core",
     "std",
 ] }
+paste = "1.0.12"
 rusb = "0.9.2"
 scroll = "0.11.0"
 serde = { version = "1", features = ["derive"] }

probe-rs/src/architecture/arm/component/dwt.rs
@@ -6,12 +6,10 @@
 //! See ARMv7-M architecture reference manual C1.8 for some additional
 //! info about this stuff.
 
-use bitfield::bitfield;
-
 use super::super::memory::romtable::CoresightComponent;
-use super::DebugRegister;
+use super::DebugComponentInterface;
 use crate::architecture::arm::{ArmError, ArmProbeInterface};
-use crate::Error;
+use crate::{memory_mapped_bitfield_register, Error};
 
 /// A struct representing a DWT unit on target.
 pub struct Dwt<'a> {
@@ -95,10 +93,10 @@ impl<'a> Dwt<'a> {
     }
 }
 
-bitfield! {
-    #[derive(Clone, Default)]
+memory_mapped_bitfield_register! {
     pub struct Ctrl(u32);
-    impl Debug;
+    0x00, "DWT/CTRL",
+    impl From;
     pub u8, numcomp, _: 31, 28;
     pub notrcpkt, _: 27;
     pub noexttrig, _: 26;
@@ -124,144 +122,48 @@ bitfield! {
 
 }
 
-impl From<u32> for Ctrl {
-    fn from(raw: u32) -> Self {
-        Ctrl(raw)
-    }
-}
+impl DebugComponentInterface for Ctrl {}
 
-impl From<Ctrl> for u32 {
-    fn from(raw: Ctrl) -> Self {
-        raw.0
-    }
-}
-
-impl DebugRegister for Ctrl {
-    const ADDRESS: u32 = 0x00;
-    const NAME: &'static str = "DWT/CTRL";
-}
-
-bitfield! {
-    #[derive(Clone, Default)]
+memory_mapped_bitfield_register! {
     pub struct Cyccnt(u32);
-    impl Debug;
+    0x04, "DWT/CYCCNT",
+    impl From;
 }
 
-impl From<u32> for Cyccnt {
-    fn from(raw: u32) -> Self {
-        Cyccnt(raw)
-    }
-}
-
-impl From<Cyccnt> for u32 {
-    fn from(raw: Cyccnt) -> Self {
-        raw.0
-    }
-}
-
-impl DebugRegister for Cyccnt {
-    const ADDRESS: u32 = 0x04;
-    const NAME: &'static str = "DWT/CYCCNT";
-}
-
-bitfield! {
-    #[derive(Clone, Default)]
+memory_mapped_bitfield_register! {
     pub struct Cpicnt(u32);
-    impl Debug;
+    0x08, "DWT/CPICNT",
+    impl From;
 }
 
-impl From<u32> for Cpicnt {
-    fn from(raw: u32) -> Self {
-        Cpicnt(raw)
-    }
-}
-
-impl From<Cpicnt> for u32 {
-    fn from(raw: Cpicnt) -> Self {
-        raw.0
-    }
-}
-
-impl DebugRegister for Cpicnt {
-    const ADDRESS: u32 = 0x08;
-    const NAME: &'static str = "DWT/CPICNT";
-}
-
-bitfield! {
-    #[derive(Clone, Default)]
+memory_mapped_bitfield_register! {
     pub struct Exccnt(u32);
-    impl Debug;
+    0x0C, "DWT/EXCCNT",
+    impl From;
 }
 
-impl From<u32> for Exccnt {
-    fn from(raw: u32) -> Self {
-        Exccnt(raw)
-    }
-}
-
-impl From<Exccnt> for u32 {
-    fn from(raw: Exccnt) -> Self {
-        raw.0
-    }
-}
-
-impl DebugRegister for Exccnt {
-    const ADDRESS: u32 = 0x0C;
-    const NAME: &'static str = "DWT/EXCCNT";
-}
-
-bitfield! {
-    #[derive(Clone, Default)]
+memory_mapped_bitfield_register! {
     pub struct Comp(u32);
-    impl Debug;
+    0x20, "DWT/COMP",
+    impl From;
     pub u32, comp, set_comp: 31, 0;
 }
 
-impl From<u32> for Comp {
-    fn from(raw: u32) -> Self {
-        Comp(raw)
-    }
-}
+impl DebugComponentInterface for Comp {}
 
-impl From<Comp> for u32 {
-    fn from(raw: Comp) -> Self {
-        raw.0
-    }
-}
-
-impl DebugRegister for Comp {
-    const ADDRESS: u32 = 0x20;
-    const NAME: &'static str = "DWT/COMP";
-}
-
-bitfield! {
-    #[derive(Clone, Default)]
+memory_mapped_bitfield_register! {
     pub struct Mask(u32);
-    impl Debug;
+    0x24, "DWT/MASK",
+    impl From;
     pub u32, mask, set_mask: 4, 0;
 }
 
-impl From<u32> for Mask {
-    fn from(raw: u32) -> Self {
-        Mask(raw)
-    }
-}
+impl DebugComponentInterface for Mask {}
 
-impl From<Mask> for u32 {
-    fn from(raw: Mask) -> Self {
-        raw.0
-    }
-}
-
-impl DebugRegister for Mask {
-    const ADDRESS: u32 = 0x24;
-    const NAME: &'static str = "DWT/MASK";
-}
-
-bitfield! {
-    #[derive(Clone, Default)]
+memory_mapped_bitfield_register! {
     pub struct Function(u32);
-    impl Debug;
+    0x28, "DWT/FUNCTION",
+    impl From;
     pub matched, _: 24;
     pub u8, datavaddr1, set_datavaddr1: 19, 16;
     pub u8, datavaddr0, set_datavaddr0: 15, 12;
@@ -276,19 +178,4 @@ bitfield! {
     pub function, set_function: 3, 0;
 }
 
-impl From<u32> for Function {
-    fn from(raw: u32) -> Self {
-        Function(raw)
-    }
-}
-
-impl From<Function> for u32 {
-    fn from(raw: Function) -> Self {
-        raw.0
-    }
-}
-
-impl DebugRegister for Function {
-    const ADDRESS: u32 = 0x28;
-    const NAME: &'static str = "DWT/FUNCTION";
-}
+impl DebugComponentInterface for Function {}

probe-rs/src/architecture/arm/component/itm.rs
@@ -3,9 +3,8 @@
 //! ITM = Instrumentation Trace Macrocell
 
 use super::super::memory::romtable::CoresightComponent;
-use super::DebugRegister;
 use crate::architecture::arm::ArmProbeInterface;
-use crate::Error;
+use crate::{Error, MemoryMappedRegister};
 
 pub const _ITM_PID: [u8; 8] = [0x1, 0xB0, 0x3b, 0x0, 0x4, 0x0, 0x0, 0x0];
 
@@ -77,7 +76,7 @@ impl<'a> Itm<'a> {
         // Enable all 32 channels.
         self.component.write_reg(
             self.interface,
-            register::ITM_TER::ADDRESS,
+            register::ITM_TER::ADDRESS_OFFSET as u32,
             register::ITM_TER::enable_all().into(),
         )?;
 
@@ -86,13 +85,14 @@ impl<'a> Itm<'a> {
 }
 
 mod register {
-    use super::super::DebugRegister;
+    use crate::memory_mapped_bitfield_register;
 
-    bitfield::bitfield! {
+    memory_mapped_bitfield_register! {
         #[allow(non_camel_case_types)]
-        #[derive(Copy, Clone)]
         pub struct ITM_TER(u32);
-        impl Debug;
+        0xE00,"ITM_TER",
+        impl From;
+        impl tpiu_continuous_formatting;
         pub stim31, set_stim31: 31;
         pub stim30, set_stim30: 30;
         pub stim29, set_stim29: 29;
@@ -135,22 +135,5 @@ mod register {
         pub fn disable_all() -> Self {
             Self(0x0000_0000)
         }
-    }
-
-    impl From<u32> for ITM_TER {
-        fn from(value: u32) -> Self {
-            Self(value)
-        }
-    }
-
-    impl From<ITM_TER> for u32 {
-        fn from(value: ITM_TER) -> Self {
-            value.0
-        }
-    }
-
-    impl DebugRegister for ITM_TER {
-        const ADDRESS: u32 = 0xE00;
-        const NAME: &'static str = "ITM_TER";
     }
 }

probe-rs/src/architecture/arm/component/mod.rs
@@ -49,19 +49,18 @@ pub enum ComponentError {
     NordicUnsupportedTPUICLKValue(u32),
 }
 
-/// A trait to be implemented on debug register types for debug component interfaces.
-pub trait DebugRegister: Clone + From<u32> + Into<u32> + Sized + std::fmt::Debug {
-    /// The address of the register.
-    const ADDRESS: u32;
-    /// The name of the register.
-    const NAME: &'static str;
-
+/// A trait to be implemented on memory mapped register types for debug component interfaces.
+pub trait DebugComponentInterface:
+    MemoryMappedRegister<u32> + Clone + From<u32> + Into<u32> + Sized + std::fmt::Debug
+{
     /// Loads the register value from the given debug component via the given core.
     fn load(
         component: &CoresightComponent,
         interface: &mut dyn ArmProbeInterface,
     ) -> Result<Self, ArmError> {
-        Ok(Self::from(component.read_reg(interface, Self::ADDRESS)?))
+        Ok(Self::from(
+            component.read_reg(interface, Self::ADDRESS_OFFSET as u32)?,
+        ))
     }
 
     /// Loads the register value from the given component in given unit via the given core.
@@ -70,9 +69,10 @@ pub trait DebugRegister: Clone + From<u32> + Into<u32> + Sized + std::fmt::Debug
         interface: &mut dyn ArmProbeInterface,
         unit: usize,
     ) -> Result<Self, ArmError> {
-        Ok(Self::from(
-            component.read_reg(interface, Self::ADDRESS + 16 * unit as u32)?,
-        ))
+        Ok(Self::from(component.read_reg(
+            interface,
+            Self::ADDRESS_OFFSET as u32 + 16 * unit as u32,
+        )?))
     }
 
     /// Stores the register value to the given debug component via the given core.
@@ -81,7 +81,7 @@ pub trait DebugRegister: Clone + From<u32> + Into<u32> + Sized + std::fmt::Debug
         component: &CoresightComponent,
         interface: &mut dyn ArmProbeInterface,
     ) -> Result<(), ArmError> {
-        component.write_reg(interface, Self::ADDRESS, self.clone().into())
+        component.write_reg(interface, Self::ADDRESS_OFFSET as u32, self.clone().into())
     }
 
     /// Stores the register value to the given component in given unit via the given core.
@@ -93,7 +93,7 @@ pub trait DebugRegister: Clone + From<u32> + Into<u32> + Sized + std::fmt::Debug
     ) -> Result<(), ArmError> {
         component.write_reg(
             interface,
-            Self::ADDRESS + 16 * unit as u32,
+            Self::ADDRESS_OFFSET as u32 + 16 * unit as u32,
             self.clone().into(),
         )
     }
@@ -375,16 +375,16 @@ pub fn remove_swv_data_trace(
 
 /// Sets TRCENA in DEMCR to begin trace generation.
 pub fn enable_tracing(core: &mut Core) -> Result<(), Error> {
-    let mut demcr = Demcr(core.read_word_32(Demcr::ADDRESS)?);
+    let mut demcr = Demcr(core.read_word_32(Demcr::get_mmio_address())?);
     demcr.set_dwtena(true);
-    core.write_word_32(Demcr::ADDRESS, demcr.into())?;
+    core.write_word_32(Demcr::get_mmio_address(), demcr.into())?;
     Ok(())
 }
 
 /// Disables TRCENA in DEMCR to disable trace generation.
 pub fn disable_swv(core: &mut Core) -> Result<(), Error> {
-    let mut demcr = Demcr(core.read_word_32(Demcr::ADDRESS)?);
+    let mut demcr = Demcr(core.read_word_32(Demcr::get_mmio_address())?);
     demcr.set_dwtena(false);
-    core.write_word_32(Demcr::ADDRESS, demcr.into())?;
+    core.write_word_32(Demcr::get_mmio_address(), demcr.into())?;
     Ok(())
 }

commit a11e0eec0b61c50ab1338d666b32ddf5108b614c

Fix comment regardig the MemoryMappedRegister address constant
The name of the constant is ADDRESS_OFFSET, not ADDRESS.

Fixes: 35e1afc0199e99337858e3fdb86dafdd4dfa12ca

probe-rs/src/core/memory_mapped_registers.rs
@@ -54,7 +54,7 @@ pub trait MemoryMappedRegister<T>: Clone + From<T> + Into<T> + Sized + std::fmt:
 /// - Default `From<u32>` and `From<MemoryMappedRegister>` impls for [`MemoryMappedRegister`] are generated.
 /// - A `bitfield!` mapping for the fields `progbufsize`, `busy`, `cmderr`, `datacount`.
 /// - `bitfield!` getters and setters for the fields as defined - See [`bitfield::bitfield!`] for more information.
-/// - A `const ADDRESS: u64 = 0x16;`.
+/// - A `const ADDRESS_OFFSET: u64 = 0x16;`.
 /// - A `const NAME: &'static str = "abstractcs";`.
 macro_rules! memory_mapped_bitfield_register {
     ($(#[$outer:meta])* $visibility:vis struct $struct_name:ident($reg_type:ty); $addr:expr, $reg_name:expr, impl From; $($rest:tt)*) => {