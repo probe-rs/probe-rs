44ffe968193a5d4a3a2c1396c9c28725ca52a5fb
9names <60134748+9names@users.noreply.github.com>
2023-10-24 19:02:48 +1100

    Remove upload function from cli benchmark

--- probe-rs/src/bin/probe-rs/cmd/benchmark.rs
+++ probe-rs/src/bin/probe-rs/cmd/benchmark.rs
@@ -1,15 +1,11 @@
 use std::{
-    env,
     num::ParseIntError,
-    process::Command,
-    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
+    time::{Duration, Instant},
 };
 
 use anyhow::Context;
 use probe_rs::MemoryInterface;
 use rand::prelude::*;
-use serde::{Deserialize, Serialize};
-use time::OffsetDateTime;
 
 use crate::util::common_options::ProbeOptions;
 
@@ -25,11 +21,6 @@ pub struct Cmd {
     /// Should be located in RAM.
     #[clap(long = "address", value_parser= parse_hex)]
     address: u64,
-    #[clap(long = "pr")]
-    pr: Option<u64>,
-
-    #[clap(long)]
-    upload: bool,
 }
 
 impl Cmd {
@@ -117,60 +108,6 @@ impl Cmd {
         } else {
             println!("Verification succesful.");
 
-            if self.upload {
-                let start = SystemTime::now();
-                let since_the_epoch = start
-                    .duration_since(UNIX_EPOCH)
-                    .expect("Time went backwards")
-                    .as_secs();
-
-                let commit_hash = String::from_utf8_lossy(
-                    &Command::new("git")
-                        .args(["rev-parse", "--short", "HEAD"])
-                        .output()
-                        .unwrap()
-                        .stdout,
-                )
-                .trim()
-                .to_string();
-
-                let commit_name = if Command::new("git")
-                    .args(["diff-index", "--quiet", "HEAD", "--"])
-                    .output()
-                    .unwrap()
-                    .status
-                    .success()
-                {
-                    commit_hash
-                } else {
-                    commit_hash + "-changed"
-                };
-
-                let client = reqwest::blocking::Client::new();
-                const BASE_URL: &str = "https://perf.probe.rs/add";
-                client
-                    .post(if let Some(pr) = self.pr {
-                        format!("{BASE_URL}?pr={pr}")
-                    } else {
-                        BASE_URL.to_string()
-                    })
-                    .json(&NewLog {
-                        probe: probe_name,
-                        chip: target_name,
-                        os: env::consts::OS.to_string(),
-                        protocol: protocol_name,
-                        protocol_speed,
-                        commit_hash: commit_name,
-                        timestamp: OffsetDateTime::from_unix_timestamp(since_the_epoch as i64)
-                            .unwrap(),
-                        kind: "ram".into(),
-                        read_speed: read_throughput as i32,
-                        write_speed: write_throughput as i32,
-                    })
-                    .send()
-                    .with_context(|| format!("Failed to upload results to {BASE_URL}"))?;
-            }
-
             Ok(())
         }
     }
@@ -178,39 +115,4 @@ impl Cmd {
 
 fn parse_hex(src: &str) -> Result<u64, ParseIntError> {
     u64::from_str_radix(src.trim_start_matches("0x"), 16)
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct NewLog {
-    pub probe: String,
-    pub chip: String,
-    pub os: String,
-    pub protocol: String,
-    pub protocol_speed: i32,
-    pub commit_hash: String,
-    #[serde(with = "timestamp")]
-    pub timestamp: OffsetDateTime,
-    pub kind: String,
-    pub read_speed: i32,
-    pub write_speed: i32,
-}
-
-mod timestamp {
-    use serde::{self, Deserialize, Deserializer, Serializer};
-    use time::OffsetDateTime;
-
-    pub fn serialize<S>(date: &OffsetDateTime, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_i64(date.unix_timestamp())
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<OffsetDateTime, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = i64::deserialize(deserializer)?;
-        Ok(OffsetDateTime::from_unix_timestamp(s).unwrap())
-    }
 }
