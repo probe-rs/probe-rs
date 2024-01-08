d31599ff9925f563c57c4a5e28388533a1820e70
Manuel Hatzl <manuel.hatzl@infineon.com>
2024-01-08 13:30:47 +0100

    fix: only capture pc if breakpoint was reached
    capturing pc requires halted core, but core must not be halted if it is not at a breakpoint.

--- probe-rs/src/architecture/arm/core/armv7m.rs
+++ probe-rs/src/architecture/arm/core/armv7m.rs
@@ -828,15 +828,15 @@ impl<'probe> CoreInterface for Armv7m<'probe> {
 
     fn step(&mut self) -> Result<CoreInformation, Error> {
         // First check if we stopped on a breakpoint, because this requires special handling before we can continue.
-        let pc_before_step = self.read_core_reg(self.program_counter().into())?;
-        let was_breakpoint = if matches!(
+        let (was_breakpoint, pc_before_step) = if matches!(
             self.state.current_state,
             CoreStatus::Halted(HaltReason::Breakpoint(_))
         ) {
+            let pc_before_step = self.read_core_reg(self.program_counter().into())?;
             self.enable_breakpoints(false)?;
-            true
+            (true, pc_before_step)
         } else {
-            false
+            (false, RegisterValue::U32(0)) // Dummy register value that won't be used
         };
 
         let mut dhcsr = Dhcsr(self.memory.read_word_32(Dhcsr::get_mmio_address())?);
