af08cf8c1e763ad77f64f2e5e2b4974ca14df3bd
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2024-01-10 13:01:21 +0100

    espusbjtag: use nusb `get_descriptor()` instead of doing a manual control request. (#2044)

--- probe-rs/src/probe/usb_util.rs
+++ probe-rs/src/probe/usb_util.rs
@@ -1,32 +1,14 @@
 use async_io::{block_on, Timer};
 use futures_lite::FutureExt;
-use nusb::{
-    transfer::{ControlIn, RequestBuffer},
-    Interface,
-};
+use nusb::{transfer::RequestBuffer, Interface};
 use std::{io, time::Duration};
 
 pub trait InterfaceExt {
-    fn read_control(&self, data: ControlIn, timeout: Duration) -> io::Result<Vec<u8>>;
     fn read_bulk(&self, endpoint: u8, buf: &mut [u8], timeout: Duration) -> io::Result<usize>;
     fn write_bulk(&self, endpoint: u8, buf: &[u8], timeout: Duration) -> io::Result<usize>;
 }
 
 impl InterfaceExt for Interface {
-    fn read_control(&self, data: ControlIn, timeout: Duration) -> io::Result<Vec<u8>> {
-        let fut = async {
-            let comp = self.control_in(data).await;
-            comp.status.map_err(io::Error::other)?;
-
-            Ok(comp.data)
-        };
-
-        block_on(fut.or(async {
-            Timer::after(timeout).await;
-            Err(std::io::ErrorKind::TimedOut.into())
-        }))
-    }
-
     fn write_bulk(&self, endpoint: u8, buf: &[u8], timeout: Duration) -> io::Result<usize> {
         let fut = async {
             let comp = self.bulk_out(endpoint, buf.to_vec()).await;
