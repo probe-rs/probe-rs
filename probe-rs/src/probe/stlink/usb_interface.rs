probe-rs/probe-rs

commit 06e181e1742ebf98355e0fea7a3d4349565a1eb8

probe: centralize "device matches selector" logic. (#2086)

probe-rs/src/probe/stlink/usb_interface.rs
@@ -5,7 +5,7 @@ use crate::probe::{stlink::StlinkError, usb_util::InterfaceExt};
 
 use std::collections::HashMap;
 
-use super::tools::{is_stlink_device, read_serial_number};
+use super::tools::is_stlink_device;
 use crate::{
     probe::{DebugProbeError, ProbeCreationError},
     DebugProbeSelector,
@@ -103,23 +103,7 @@ impl StLinkUsbDevice {
         let device = nusb::list_devices()
             .map_err(ProbeCreationError::Usb)?
             .filter(is_stlink_device)
-            .find(|device| {
-                // First match the VID & PID.
-                if selector.vendor_id == device.vendor_id()
-                    && selector.product_id == device.product_id()
-                {
-                    // If the VID & PID match, match the serial if one was given.
-                    if let Some(serial) = selector.serial_number.as_ref() {
-                        let sn_str = read_serial_number(device);
-                        sn_str.as_ref() == Some(serial)
-                    } else {
-                        // If no serial was given, the VID & PID match is enough; return the device.
-                        true
-                    }
-                } else {
-                    false
-                }
-            })
+            .find(|device| selector.matches(device))
             .ok_or(ProbeCreationError::NotFound)?;
 
         let info = USB_PID_EP_MAP[&device.product_id()].clone();

probe-rs/src/probe/wlink/usb_interface.rs
@@ -22,10 +22,7 @@ impl WchLinkUsbDevice {
     pub fn new_from_selector(selector: &DebugProbeSelector) -> Result<Self, ProbeCreationError> {
         let device = nusb::list_devices()
             .map_err(ProbeCreationError::Usb)?
-            .filter(|device| {
-                device.vendor_id() == selector.vendor_id
-                    && device.product_id() == selector.product_id
-            })
+            .filter(|device| selector.matches(device))
             .find(|device| get_wlink_info(device).is_some())
             .ok_or(ProbeCreationError::NotFound)?;
 

probe-rs/src/probe.rs
@@ -33,6 +33,7 @@ use crate::{
     Permissions,
 };
 use crate::{Lister, Session};
+use nusb::DeviceInfo;
 use probe_rs_target::ScanChainElement;
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -758,6 +759,18 @@ pub struct DebugProbeSelector {
     pub product_id: u16,
     /// The the serial number of the debug probe to be used.
     pub serial_number: Option<String>,
+}
+
+impl DebugProbeSelector {
+    pub(crate) fn matches(&self, info: &DeviceInfo) -> bool {
+        info.vendor_id() == self.vendor_id
+            && info.product_id() == self.product_id
+            && self
+                .serial_number
+                .as_ref()
+                .map(|s| info.serial_number() == Some(s))
+                .unwrap_or(true)
+    }
 }
 
 impl TryFrom<&str> for DebugProbeSelector {

probe-rs/src/probe/cmsisdap/tools.rs
@@ -228,18 +228,6 @@ pub fn open_v2_device(device_info: &DeviceInfo) -> Option<CmsisDapDevice> {
     None
 }
 
-fn device_matches(device: &DeviceInfo, selector: &DebugProbeSelector) -> bool {
-    if device.vendor_id() == selector.vendor_id && device.product_id() == selector.product_id {
-        if selector.serial_number.is_some() {
-            device.serial_number() == selector.serial_number.as_deref()
-        } else {
-            true
-        }
-    } else {
-        false
-    }
-}
-
 /// Attempt to open the given DebugProbeInfo in CMSIS-DAP v2 mode if possible,
 /// otherwise in v1 mode.
 pub fn open_device_from_selector(
@@ -263,7 +251,7 @@ pub fn open_device_from_selector(
         for device in devices {
             tracing::trace!("Trying device {:?}", device);
 
-            if device_matches(&device, selector) {
+            if selector.matches(&device) {
                 hid_device_info = get_cmsisdap_info(&device);
 
                 if hid_device_info.is_some() {

probe-rs/src/probe/espusbjtag/protocol.rs
@@ -75,22 +75,7 @@ impl ProtocolHandler {
         let device = nusb::list_devices()
             .map_err(ProbeCreationError::Usb)?
             .filter(is_espjtag_device)
-            .find(|device| {
-                // First match the VID & PID.
-                if selector.vendor_id == device.vendor_id()
-                    && selector.product_id == device.product_id()
-                {
-                    // If the VID & PID match, match the serial if one was given.
-                    if let Some(serial) = &selector.serial_number {
-                        device.serial_number() == Some(serial)
-                    } else {
-                        // If no serial was given, the VID & PID match is enough; return the device.
-                        true
-                    }
-                } else {
-                    false
-                }
-            })
+            .find(|device| selector.matches(device))
             .ok_or(ProbeCreationError::NotFound)?;
 
         let device_handle = device.open().map_err(ProbeCreationError::Usb)?;

probe-rs/src/probe/ftdi/mod.rs
@@ -202,14 +202,7 @@ impl ProbeDriver for FtdiProbeSource {
         let device = match nusb::list_devices() {
             Ok(devices) => {
                 let mut matched = None;
-                for device in devices {
-                    // Is this the device we're looking for?
-                    if (device.product_id(), device.vendor_id())
-                        != (selector.product_id, selector.vendor_id)
-                    {
-                        continue;
-                    }
-
+                for device in devices.filter(|info| selector.matches(info)) {
                     // FTDI devices don't have serial numbers, so we can only match on VID/PID.
                     // Bail if we find more than one matching device.
                     if matched.is_some() {

probe-rs/src/probe/jlink/mod.rs
@@ -44,15 +44,7 @@ impl std::fmt::Debug for JLinkSource {
 impl ProbeDriver for JLinkSource {
     fn open(&self, selector: &DebugProbeSelector) -> Result<Box<dyn DebugProbe>, DebugProbeError> {
         let mut jlinks = jaylink::scan_usb()?
-            .filter(|usb_info| {
-                usb_info.vendor_id() == selector.vendor_id
-                    && usb_info.product_id() == selector.product_id
-                    && selector
-                        .serial_number
-                        .as_ref()
-                        .map(|s| usb_info.serial_number() == Some(s))
-                        .unwrap_or(true)
-            })
+            .filter(|info| selector.matches(info))
             .collect::<Vec<_>>();
 
         if jlinks.is_empty() {

commit e4e369ad4be849991049fb334e735910cc7607d5

stlink: use hex-encoded serial to match selectors.
Regression introduced in #2086

Fixes #2100

probe-rs/src/probe/stlink/usb_interface.rs
@@ -1,3 +1,4 @@
+use nusb::DeviceInfo;
 use once_cell::sync::Lazy;
 use std::time::Duration;
 
@@ -5,7 +6,7 @@ use crate::probe::{stlink::StlinkError, usb_util::InterfaceExt};
 
 use std::collections::HashMap;
 
-use super::tools::is_stlink_device;
+use super::tools::{is_stlink_device, read_serial_number};
 use crate::{
     probe::{DebugProbeError, ProbeCreationError},
     DebugProbeSelector,
@@ -97,13 +98,27 @@ pub(crate) trait StLinkUsb: std::fmt::Debug {
     ) -> Result<usize, DebugProbeError>;
 }
 
+// Copy of `Selector::matches` except it uses the stlink-specific read_serial_number
+// to handle the broken stlink-v2 serial numbers that need hex-encoding.
+fn selector_matches(selector: &DebugProbeSelector, info: &DeviceInfo) -> bool {
+    let res = info.vendor_id() == selector.vendor_id
+        && info.product_id() == selector.product_id
+        && selector
+            .serial_number
+            .as_ref()
+            .map(|s| read_serial_number(info).as_ref() == Some(s))
+            .unwrap_or(true);
+
+    res
+}
+
 impl StLinkUsbDevice {
     /// Creates and initializes a new USB device.
     pub fn new_from_selector(selector: &DebugProbeSelector) -> Result<Self, ProbeCreationError> {
         let device = nusb::list_devices()
             .map_err(ProbeCreationError::Usb)?
             .filter(is_stlink_device)
-            .find(|device| selector.matches(device))
+            .find(|device| selector_matches(selector, device))
             .ok_or(ProbeCreationError::NotFound)?;
 
         let info = USB_PID_EP_MAP[&device.product_id()].clone();