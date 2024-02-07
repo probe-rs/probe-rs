22e13c5a7ffc52f231f8ec4f03480868dc8df237
Dániel Buga <bugadani@gmail.com>
2024-02-07 22:15:54 +0100

    Read C numbers with arbitrary lengths

--- probe-rs/src/debug/language/c.rs
+++ probe-rs/src/debug/language/c.rs
@@ -79,37 +79,97 @@ fn read_c_char(
     })
 }
 
+/// A very naive implementation of printing an arbitrary length number.
+fn print_arbitrary_length(is_signed: bool, num: &mut [u8]) -> String {
+    let prefix = if is_signed {
+        let negative = num.last().map_or(false, |&x| x & 0x80 != 0);
+
+        if negative {
+            // Two's complement
+            let mut carry = true;
+            for byte in num.iter_mut() {
+                *byte = !*byte;
+                let (new, overflow) = byte.overflowing_add(carry as u8);
+                *byte = new;
+                carry = overflow;
+            }
+        }
+        if negative {
+            "-"
+        } else {
+            ""
+        }
+    } else {
+        ""
+    };
+
+    // in a loop, we divide the number by 10 and print the remainder digit
+    let mut out = String::new();
+    while num.iter().any(|&x| x != 0) {
+        // divide byte-by-byte by 10.
+        // We could divide by 100 but that way we may end up with a leading 0 we have to remove
+        let mut carry = 0;
+        for byte in num.iter_mut().rev() {
+            let val = *byte as u32 + carry * 256;
+            *byte = (val / 10) as u8;
+            carry = val % 10;
+        }
+        out.insert(0, char::from_digit(carry, 10).unwrap());
+    }
+
+    if out.is_empty() {
+        out.push('0');
+    }
+
+    out.insert_str(0, prefix);
+
+    out
+}
+
 fn read_unsigned_int(
     variable: &Variable,
     memory: &mut dyn MemoryInterface,
 ) -> Result<String, DebugError> {
-    let mut buff = 0usize.to_le_bytes();
-    memory.read(
-        variable.memory_location.memory_address()?,
-        &mut buff[..variable.byte_size.unwrap_or(1) as usize],
-    )?;
+    let mut buff = vec![0u8; variable.byte_size.unwrap_or(1) as usize];
+    memory.read(variable.memory_location.memory_address()?, &mut buff)?;
 
-    Ok(usize::from_le_bytes(buff).to_string())
+    Ok(print_arbitrary_length(false, &mut buff))
 }
 
 fn read_signed_int(
     variable: &Variable,
     memory: &mut dyn MemoryInterface,
 ) -> Result<String, DebugError> {
-    let mut buff = 0usize.to_le_bytes();
-    memory.read(
-        variable.memory_location.memory_address()?,
-        &mut buff[..variable.byte_size.unwrap_or(1) as usize],
-    )?;
-    let unsigned = usize::from_le_bytes(buff);
-    // sign extend
-    let shift = (std::mem::size_of::<isize>() - variable.byte_size.unwrap_or(1) as usize) * 8;
-    let signed = (unsigned << shift) as isize >> shift;
-    Ok(signed.to_string())
+    let mut buff = vec![0u8; variable.byte_size.unwrap_or(1) as usize];
+    memory.read(variable.memory_location.memory_address()?, &mut buff)?;
+
+    Ok(print_arbitrary_length(true, &mut buff))
 }
 
 fn read_f32(variable: &Variable, memory: &mut dyn MemoryInterface) -> Result<String, DebugError> {
     let mut buff = [0u8; 4];
     memory.read(variable.memory_location.memory_address()?, &mut buff)?;
     Ok(f32::from_le_bytes(buff).to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_print_arbitrary_length_positive() {
+        assert_eq!(print_arbitrary_length(true, &mut []), "0");
+
+        let mut buff = [0x78, 0x56, 0x34, 0x12];
+        assert_eq!(print_arbitrary_length(false, &mut buff), "305419896");
+
+        let mut buff = [0xFC, 0xFF, 0xFF];
+        assert_eq!(print_arbitrary_length(false, &mut buff), "16777212");
+
+        let mut buff = [0x78, 0x56, 0x34, 0x12];
+        assert_eq!(print_arbitrary_length(true, &mut buff), "305419896");
+
+        let mut buff = [0xFC, 0xFF, 0xFF];
+        assert_eq!(print_arbitrary_length(true, &mut buff), "-4");
+    }
 }
