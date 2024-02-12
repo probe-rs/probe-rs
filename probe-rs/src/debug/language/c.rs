d5666b4172e266c525c2c37769ccf88187cdd674
Dániel Buga <bugadani@gmail.com>
2024-02-12 10:39:43 +0100

    Rework C values into Value impls

--- probe-rs/src/debug/language/c.rs
+++ probe-rs/src/debug/language/c.rs
@@ -23,15 +23,15 @@ impl ProgrammingLanguage for C {
             }
 
             VariableType::Base(type_name) => match type_name.as_str() {
-                "_Bool" => read_unsigned_int(variable, memory).into(),
-                "char" => read_c_char(variable, memory).into(),
+                "_Bool" => UnsignedInt::get_value(variable, memory, variable_cache).into(),
+                "char" => CChar::get_value(variable, memory, variable_cache).into(),
 
                 "unsigned char" | "unsigned int" | "short unsigned int" | "long unsigned int" => {
-                    read_unsigned_int(variable, memory).into()
+                    UnsignedInt::get_value(variable, memory, variable_cache).into()
                 }
                 "signed char" | "int" | "short int" | "long int" | "signed int"
                 | "short signed int" | "long signed int" => {
-                    read_signed_int(variable, memory).into()
+                    SignedInt::get_value(variable, memory, variable_cache).into()
                 }
 
                 "float" => match variable.byte_size {
@@ -55,14 +55,14 @@ impl ProgrammingLanguage for C {
     ) -> Result<(), DebugError> {
         match &variable.type_name {
             VariableType::Base(name) => match name.as_str() {
-                "_Bool" => write_unsigned_int(variable, memory, new_value),
-                "char" => write_c_char(variable, memory, new_value),
+                "_Bool" => UnsignedInt::update_value(variable, memory, new_value),
+                "char" => CChar::update_value(variable, memory, new_value),
                 "unsigned char" | "unsigned int" | "short unsigned int" | "long unsigned int" => {
-                    write_unsigned_int(variable, memory, new_value)
+                    UnsignedInt::update_value(variable, memory, new_value)
                 }
                 "signed char" | "int" | "short int" | "long int" | "signed int"
                 | "short signed int" | "long signed int" => {
-                    write_signed_int(variable, memory, new_value)
+                    SignedInt::update_value(variable, memory, new_value)
                 }
                 "float" => f32::update_value(variable, memory, new_value),
                 // TODO: doubles
@@ -91,169 +91,152 @@ impl ProgrammingLanguage for C {
     }
 }
 
-fn read_c_char(
-    variable: &Variable,
-    memory: &mut dyn MemoryInterface,
-) -> Result<String, DebugError> {
-    let mut buff = 0u8;
-    memory.read(
-        variable.memory_location.memory_address()?,
-        std::slice::from_mut(&mut buff),
-    )?;
+struct CChar(u8);
 
-    Ok(if buff.is_ascii() {
-        (buff as char).to_string()
-    } else {
-        format!("\\x{:02x}", buff)
-    })
-}
-
-fn write_c_char(
-    variable: &Variable,
-    memory: &mut dyn MemoryInterface,
-    new_value: &str,
-) -> Result<(), DebugError> {
-    fn input_error(value: &str) -> DebugError {
-        DebugError::UnwindIncompleteResults {
-            message: format!("Invalid value for char: {value}. Please provide a single character."),
-        }
-    }
-
-    // TODO: what do we want to support here exactly? This is now symmetrical with read_c_char
-    // but we could be somewhat smarter, too.
-    let new_value = if new_value.len() == 1 && new_value.is_ascii() {
-        new_value.as_bytes()[0]
-    } else if new_value.starts_with("\\x") && [3, 4].contains(&new_value.len()) {
-        u8::from_str_radix(&new_value[2..], 16).map_err(|_| input_error(new_value))?
-    } else {
-        return Err(input_error(new_value));
-    };
-
-    memory.write(variable.memory_location.memory_address()?, &[new_value])?;
-
-    Ok(())
-}
-
-/// A very naive implementation of printing an arbitrary length number.
-fn print_arbitrary_length(is_signed: bool, num: &mut [u8]) -> String {
-    let prefix = if is_signed {
-        let negative = num.last().map_or(false, |&x| x & 0x80 != 0);
-        if negative {
-            twos_complement(num);
-            "-"
+impl ToString for CChar {
+    fn to_string(&self) -> String {
+        let c = self.0;
+        if c.is_ascii() {
+            (c as char).to_string()
         } else {
-            ""
+            format!("\\x{:02x}", c)
         }
-    } else {
-        ""
-    };
-
-    // in a loop, we divide the number by 10 and print the remainder digit
-    let mut out = String::new();
-    while num.iter().any(|&x| x != 0) {
-        let carry = divide(num, 10);
-        out.insert(0, char::from_digit(carry, 10).unwrap());
-    }
-
-    if out.is_empty() {
-        out.push('0');
-    }
-
-    out.insert_str(0, prefix);
-
-    out
-}
-
-// Divide byte-by-byte
-fn divide(num: &mut [u8], by: u8) -> u32 {
-    let by = by as u32;
-
-    let mut carry = 0;
-    for byte in num.iter_mut().rev() {
-        let val = *byte as u32 + carry * 256;
-        *byte = (val / by) as u8;
-        carry = val % by;
-    }
-
-    carry
-}
-
-fn twos_complement(num: &mut [u8]) {
-    let mut carry = true;
-    for byte in num.iter_mut() {
-        *byte = !*byte;
-        let (new, overflow) = byte.overflowing_add(carry as u8);
-        *byte = new;
-        carry = overflow;
     }
 }
 
-fn read_unsigned_int(
-    variable: &Variable,
-    memory: &mut dyn MemoryInterface,
-) -> Result<String, DebugError> {
-    let mut buff = vec![0u8; variable.byte_size.unwrap_or(1) as usize];
-    memory.read(variable.memory_location.memory_address()?, &mut buff)?;
+impl Value for CChar {
+    fn get_value(
+        variable: &Variable,
+        memory: &mut dyn MemoryInterface,
+        _variable_cache: &VariableCache,
+    ) -> Result<Self, DebugError>
+    where
+        Self: Sized,
+    {
+        let mut buff = 0u8;
+        memory.read(
+            variable.memory_location.memory_address()?,
+            std::slice::from_mut(&mut buff),
+        )?;
 
-    Ok(print_arbitrary_length(false, &mut buff))
+        Ok(Self(buff))
+    }
+
+    fn update_value(
+        variable: &Variable,
+        memory: &mut dyn MemoryInterface,
+        new_value: &str,
+    ) -> Result<(), DebugError> {
+        fn input_error(value: &str) -> DebugError {
+            DebugError::UnwindIncompleteResults {
+                message: format!(
+                    "Invalid value for char: {value}. Please provide a single character."
+                ),
+            }
+        }
+
+        // TODO: what do we want to support here exactly? This is now symmetrical with get_value
+        // but we could be somewhat smarter, too.
+        let new_value = if new_value.len() == 1 && new_value.is_ascii() {
+            new_value.as_bytes()[0]
+        } else if new_value.starts_with("\\x") && [3, 4].contains(&new_value.len()) {
+            u8::from_str_radix(&new_value[2..], 16).map_err(|_| input_error(new_value))?
+        } else {
+            return Err(input_error(new_value));
+        };
+
+        memory.write(variable.memory_location.memory_address()?, &[new_value])?;
+
+        Ok(())
+    }
 }
 
-fn read_signed_int(
-    variable: &Variable,
-    memory: &mut dyn MemoryInterface,
-) -> Result<String, DebugError> {
-    let mut buff = vec![0u8; variable.byte_size.unwrap_or(1) as usize];
-    memory.read(variable.memory_location.memory_address()?, &mut buff)?;
+struct UnsignedInt(u128);
 
-    Ok(print_arbitrary_length(true, &mut buff))
+impl ToString for UnsignedInt {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
 }
 
-fn write_unsigned_int(
-    variable: &Variable,
-    memory: &mut dyn MemoryInterface,
-    new_value: &str,
-) -> Result<(), DebugError> {
-    let buff = u128::parse_to_bytes(new_value)?;
+impl Value for UnsignedInt {
+    fn get_value(
+        variable: &Variable,
+        memory: &mut dyn MemoryInterface,
+        _variable_cache: &VariableCache,
+    ) -> Result<Self, DebugError>
+    where
+        Self: Sized,
+    {
+        let mut buff = [0u8; 16];
+        let bytes = variable.byte_size.unwrap_or(1).min(16) as usize;
+        memory.read(
+            variable.memory_location.memory_address()?,
+            &mut buff[..bytes],
+        )?;
 
-    // TODO: check that value actually fits into `bytes` number of bytes
-    let bytes = variable.byte_size.unwrap_or(1) as usize;
-    memory.write_8(variable.memory_location.memory_address()?, &buff[..bytes])?;
+        Ok(Self(u128::from_le_bytes(buff)))
+    }
 
-    Ok(())
+    fn update_value(
+        variable: &Variable,
+        memory: &mut dyn MemoryInterface,
+        new_value: &str,
+    ) -> Result<(), DebugError> {
+        let buff = u128::parse_to_bytes(new_value)?;
+
+        // TODO: check that value actually fits into `bytes` number of bytes
+        let bytes = variable.byte_size.unwrap_or(1) as usize;
+        memory.write_8(variable.memory_location.memory_address()?, &buff[..bytes])?;
+
+        Ok(())
+    }
 }
 
-fn write_signed_int(
-    variable: &Variable,
-    memory: &mut dyn MemoryInterface,
-    new_value: &str,
-) -> Result<(), DebugError> {
-    let buff = i128::parse_to_bytes(new_value)?;
+struct SignedInt(i128);
 
-    // TODO: check that value actually fits into `bytes` number of bytes
-    let bytes = variable.byte_size.unwrap_or(1) as usize;
-    memory.write_8(variable.memory_location.memory_address()?, &buff[..bytes])?;
-
-    Ok(())
+impl ToString for SignedInt {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
+impl Value for SignedInt {
+    fn get_value(
+        variable: &Variable,
+        memory: &mut dyn MemoryInterface,
+        _variable_cache: &VariableCache,
+    ) -> Result<Self, DebugError>
+    where
+        Self: Sized,
+    {
+        let mut buff = [0u8; 16];
+        let bytes = variable.byte_size.unwrap_or(1).min(16) as usize;
+        memory.read(
+            variable.memory_location.memory_address()?,
+            &mut buff[..bytes],
+        )?;
 
-    #[test]
-    fn test_print_arbitrary_length_positive() {
-        assert_eq!(print_arbitrary_length(true, &mut []), "0");
+        // sign extend
+        let negative = buff[bytes - 1] >= 0x80;
+        if negative {
+            buff[bytes..].fill(0xFF);
+        }
 
-        let mut buff = [0x78, 0x56, 0x34, 0x12];
-        assert_eq!(print_arbitrary_length(false, &mut buff), "305419896");
+        Ok(Self(i128::from_le_bytes(buff)))
+    }
 
-        let mut buff = [0xFC, 0xFF, 0xFF];
-        assert_eq!(print_arbitrary_length(false, &mut buff), "16777212");
+    fn update_value(
+        variable: &Variable,
+        memory: &mut dyn MemoryInterface,
+        new_value: &str,
+    ) -> Result<(), DebugError> {
+        let buff = i128::parse_to_bytes(new_value)?;
 
-        let mut buff = [0x78, 0x56, 0x34, 0x12];
-        assert_eq!(print_arbitrary_length(true, &mut buff), "305419896");
+        // TODO: check that value actually fits into `bytes` number of bytes
+        let bytes = variable.byte_size.unwrap_or(1) as usize;
+        memory.write_8(variable.memory_location.memory_address()?, &buff[..bytes])?;
 
-        let mut buff = [0xFC, 0xFF, 0xFF];
-        assert_eq!(print_arbitrary_length(true, &mut buff), "-4");
+        Ok(())
     }
 }
