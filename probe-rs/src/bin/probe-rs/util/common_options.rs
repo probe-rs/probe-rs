47f41b9da29814fbf25a44cc77ece598ab104658
JackN <noppej@hotmail.com>
2024-02-22 19:25:19 -0500

    Remove unnecessary `:` in message.

--- probe-rs/src/bin/probe-rs/util/common_options.rs
+++ probe-rs/src/bin/probe-rs/util/common_options.rs
@@ -577,11 +577,11 @@ impl From<OperationError> for DebuggerError {
                 probe_rs::Error::Timeout => {
                     if !connect_under_reset {
                         DebuggerError::UserMessage(
-                        format!("{source} : This can happen if the target is not in a state where it can be attached to. A hard reset during attaching might help. This will reset the entire chip. If your probe supports this option, try setting `connectUnderReset=true` in your `launch.json`."),
+                        format!("{source} This can happen if the target is not in a state where it can be attached to. A hard reset during attaching might help. This will reset the entire chip. If your probe supports this option, try setting `connectUnderReset=true` in your `launch.json`."),
                     )
                     } else {
                         DebuggerError::UserMessage(
-                        format!("{source} : This can happen if your probe does not support the `connectUnderReset` option. Try setting `connectUnderReset=false` in your `launch.json`."),
+                        format!("{source} This can happen if your probe does not support the `connectUnderReset` option. Try setting `connectUnderReset=false` in your `launch.json`."),
                     )
                     }
                 }
