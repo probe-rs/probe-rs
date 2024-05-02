d27fd39356ccbfb966a69e7a50e090264ae841b1
JackN <noppej@hotmail.com>
2024-05-02 09:38:20 -0400

    Revert "Merge branch 'unwind_reset_handler' of https://github.com/probe-rs/probe-rs into unwind_reset_handler"
    This reverts commit 9e2ace3bbb60d11eba40b4d627045a5e663f951c, reversing
    changes made to 039320792301ae2b4959cf7e41f28d760b928e3b.

--- probe-rs/src/debug/debug_info.rs
+++ probe-rs/src/debug/debug_info.rs
@@ -545,7 +545,11 @@ impl DebugInfo {
         // Unwind [StackFrame]'s for as long as we can unwind a valid PC value.
         'unwind: while let Some(frame_pc_register_value) =
             unwind_registers.get_program_counter().and_then(|pc| {
-                (pc.is_zero() | pc.is_max_value()).and_then(|| pc.value)
+                if pc.is_zero() | pc.is_max_value() {
+                    None
+                } else {
+                    pc.value
+                }
             })
         {
             // PART 0: The first step is to determine the exception context for the current PC.
