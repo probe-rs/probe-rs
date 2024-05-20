83fad009c9db273410bcb644b49f3b389b076a37
JackN <noppej@hotmail.com>
2024-05-20 16:44:20 -0400

    Final cleanup after testing.

--- probe-rs-tools/src/bin/probe-rs/cmd/dap_server/server/logger.rs
+++ probe-rs-tools/src/bin/probe-rs/cmd/dap_server/server/logger.rs
@@ -2,7 +2,7 @@ use crate::cmd::dap_server::{
     debug_adapter::{dap::adapter::DebugAdapter, protocol::ProtocolAdapter},
     DebuggerError,
 };
-use probe_rs::debug::DebugError;
+use anyhow::anyhow;
 use std::{
     fs::File,
     io::{stderr, BufRead, BufReader, LineWriter, SeekFrom, Write},
@@ -23,7 +23,9 @@ use tracing_subscriber::{
 pub(crate) struct DebugLogger {
     /// This is a temporary file, created inside of `std::env::temp_dir()`,
     /// and will be automatically removed by the OS when the last handle to it is closed.
-    buffer_file: Option<Mutex<File>>,
+    ///  - When the DAP server is running, the tracing messages are sent to the console.
+    ///  - When the DAP server exits, the remaining messages in this buffer are sent to `stderr`.
+    buffer_file: Mutex<File>,
     /// Keep track of where we are in the file, so that we don't re-read or overwrite data.
     seek_pointer: SeekFrom,
     /// We need to hold onto the tracing `DefaultGuard` for the `lifetime of DebugLogger`.
@@ -53,20 +55,8 @@ impl MakeWriter<'_> for DebugLogger {
 impl DebugLogger {
     /// Create a new DebugTraceFile instance
     pub(crate) fn new(log_file: Option<&Path>) -> Result<Self, DebuggerError> {
-        // A temporary storage for tracing messages to be held,
-        // will only be created if we are not using a logging to a file. In that case,
-        //  - When the DAP server is running, the tracing messages are sent to the console.
-        //  - When the DAP server exits, the remaining messages in this buffer are sent to `stderr`.
-        let buffer_file_inner = if log_file.is_some() {
-            None
-        } else {
-            // If we don't have a log file, and we can't create a buffer file, then we can't log anything,
-            // so we exit early, and tell the user what went wrong.
-            Some(tempfile()?)
-        };
-        let buffer_file = buffer_file_inner.map(Mutex::new);
         let mut debug_logger = Self {
-            buffer_file,
+            buffer_file: Mutex::new(tempfile()?),
             seek_pointer: SeekFrom::Start(0),
             log_default_guard: None,
         };
@@ -77,11 +67,20 @@ impl DebugLogger {
 
     /// Try to clone the buffer file handle with exclusive access, so that it can be used by either
     /// the DebugAdapter to send messages to the DAP client, or the `tracing` module to record log data.
-    pub(crate) fn buffer_file_handle(&self) -> Option<File> {
+    pub(crate) fn buffer_file_handle(&self) -> Result<File, DebuggerError> {
         self.buffer_file
-            .as_ref()
-            .and_then(|buffer_file_mutex| buffer_file_mutex.lock().ok())
-            .and_then(|buffer_file: std::sync::MutexGuard<File>| buffer_file.try_clone().ok())
+            .lock()
+            .map_err(|error| {
+                DebuggerError::Other(anyhow!(
+                    "Failed to get a lock on the buffer file. {error:?}"
+                ))
+            })?
+            .try_clone()
+            .map_err(|_| {
+                DebuggerError::Other(anyhow!(
+                    "Failed to get a new file instance to the buffer file."
+                ))
+            })
     }
 
     /// Flush the buffer to the DAP client's Debug Console
@@ -89,41 +88,33 @@ impl DebugLogger {
         &mut self,
         debug_adapter: &mut DebugAdapter<impl ProtocolAdapter>,
     ) -> Result<(), DebuggerError> {
-        if let Some(read_from_log) = self.buffer_file_handle() {
-            let mut tracing_log_handle = BufReader::new(read_from_log.try_clone()?);
-            std::io::Seek::seek(&mut tracing_log_handle, self.seek_pointer)?;
-            let mut buffer_lines = tracing_log_handle.lines();
-            while let Some(Ok(next_line)) = buffer_lines.next() {
-                debug_adapter.log_to_console(next_line);
-            }
-            // Update the seek_pointer to the end of the file, so that we don't re-read the same lines.
-            let mut truncate_file = read_from_log.try_clone()?;
-            let later_read_pos =
-                std::io::Seek::seek(&mut truncate_file, std::io::SeekFrom::End(0))?;
-            self.seek_pointer = std::io::SeekFrom::Start(later_read_pos);
-        } else {
-            eprintln!("Failed to get a lock on the buffer file.");
+        let read_from_log = self.buffer_file_handle()?;
+        let mut tracing_log_handle = BufReader::new(read_from_log.try_clone()?);
+        std::io::Seek::seek(&mut tracing_log_handle, self.seek_pointer)?;
+        let mut buffer_lines = tracing_log_handle.lines();
+        while let Some(Ok(next_line)) = buffer_lines.next() {
+            debug_adapter.log_to_console(next_line);
         }
+        // Update the seek_pointer to the end of the file, so that we don't re-read the same lines.
+        let mut truncate_file = read_from_log.try_clone()?;
+        let later_read_pos = std::io::Seek::seek(&mut truncate_file, std::io::SeekFrom::End(0))?;
+        self.seek_pointer = std::io::SeekFrom::Start(later_read_pos);
         Ok(())
     }
 
     /// Flush the buffer to the stderr
-    pub(crate) fn flush(&mut self) -> Result<(), DebugError> {
-        if let Some(read_from_log) = self.buffer_file_handle() {
-            let mut tracing_log_handle = BufReader::new(read_from_log.try_clone()?);
-            std::io::Seek::seek(&mut tracing_log_handle, self.seek_pointer)?;
-            let mut buffer_lines = tracing_log_handle.lines();
-            while let Some(Ok(next_line)) = buffer_lines.next() {
-                eprintln!("{}", next_line);
-            }
-            // Update the seek_pointer to the end of the file, so that we don't re-read the same lines.
-            let mut truncate_file = read_from_log.try_clone()?;
-            let later_read_pos =
-                std::io::Seek::seek(&mut truncate_file, std::io::SeekFrom::End(0))?;
-            self.seek_pointer = std::io::SeekFrom::Start(later_read_pos);
-        } else {
-            eprintln!("Failed to get a lock on the buffer file.");
+    pub(crate) fn flush(&mut self) -> Result<(), DebuggerError> {
+        let read_from_log = self.buffer_file_handle()?;
+        let mut tracing_log_handle = BufReader::new(read_from_log.try_clone()?);
+        std::io::Seek::seek(&mut tracing_log_handle, self.seek_pointer)?;
+        let mut buffer_lines = tracing_log_handle.lines();
+        while let Some(Ok(next_line)) = buffer_lines.next() {
+            eprintln!("{}", next_line);
         }
+        // Update the seek_pointer to the end of the file, so that we don't re-read the same lines.
+        let mut truncate_file = read_from_log.try_clone()?;
+        let later_read_pos = std::io::Seek::seek(&mut truncate_file, std::io::SeekFrom::End(0))?;
+        self.seek_pointer = std::io::SeekFrom::Start(later_read_pos);
         Ok(())
     }
 
@@ -151,7 +142,7 @@ impl DebugLogger {
                     log_path.display()
                 );
 
-                // The log file will respect the RUST_LOG environment variable as a filter.
+                // Subscriber for the designated log file.
                 let file_subscriber = tracing_subscriber::fmt::layer()
                     .json()
                     .with_file(true)
@@ -194,26 +185,17 @@ impl DebugLogger {
                     &environment_filter.to_string()
                 );
 
-                // If no log file desitination is specified, we log based on the RUST_LOG setting.
-                // The `Option<Layer>` can be used interchangeably with the `Layer` type.
-                let buffer_layer = if self.buffer_file.is_some() {
-                    Some(
-                        tracing_subscriber::fmt::layer()
-                            .compact()
-                            .with_ansi(false)
-                            .without_time()
-                            .with_line_number(true)
-                            .with_span_events(FmtSpan::FULL)
-                            .with_writer(BoxMakeWriter::new(self.make_writer()))
-                            .with_filter(environment_filter),
-                    )
-                } else {
-                    return Err(DebuggerError::UserMessage(format!(
-                        "{}{}",
-                        "Unexpected error in creating a buffer file for log messages. ",
-                        "Please use the `logFile` or `logToDir` options."
-                    )));
-                };
+                // If no log file desitination is specified, send logs via the buffer file, to the DAP
+                // client's Debug Console.
+                let buffer_layer = tracing_subscriber::fmt::layer()
+                    .compact()
+                    .with_ansi(false)
+                    .without_time()
+                    .with_line_number(true)
+                    .with_span_events(FmtSpan::FULL)
+                    .with_writer(BoxMakeWriter::new(self.make_writer()))
+                    .with_filter(environment_filter);
+
                 let log_default_guard = tracing_subscriber::registry()
                     .with(buffer_layer)
                     .set_default();
@@ -227,19 +209,16 @@ impl DebugLogger {
     /// We can send messages directly to the console, irrespective of log levels, by writing to the `buffer_file`.
     /// If no `buffer_file` is available, we write to `stderr`.
     pub(crate) fn log_to_console(&mut self, message: &str) -> Result<(), DebuggerError> {
-        if let Some(read_from_log) = self.buffer_file_handle() {
-            let mut tracing_log_append_handle = read_from_log.try_clone()?;
-            std::io::Seek::seek(&mut tracing_log_append_handle, std::io::SeekFrom::End(0))?;
-            let mut tracing_log_handle = LineWriter::new(tracing_log_append_handle);
-            tracing_log_handle.write_all(format!("probe-rs-debug: {}\n", message).as_bytes())?;
-            tracing_log_handle.flush()?;
-            // Make sure we reset the `seek_pointer` to what it was before we wrote to the file,
-            // so that we can send this data to the console.
-            let mut truncate_file = read_from_log.try_clone()?;
-            let _ = std::io::Seek::seek(&mut truncate_file, self.seek_pointer)?;
-        } else {
-            eprintln!("probe-rs-debug: {}", message);
-        }
+        let read_from_log = self.buffer_file_handle()?;
+        let mut tracing_log_append_handle = read_from_log.try_clone()?;
+        std::io::Seek::seek(&mut tracing_log_append_handle, std::io::SeekFrom::End(0))?;
+        let mut tracing_log_handle = LineWriter::new(tracing_log_append_handle);
+        tracing_log_handle.write_all(format!("probe-rs-debug: {}\n", message).as_bytes())?;
+        tracing_log_handle.flush()?;
+        // Make sure we reset the `seek_pointer` to what it was before we wrote to the file,
+        // so that we can send this data to the console.
+        let mut truncate_file = read_from_log.try_clone()?;
+        let _ = std::io::Seek::seek(&mut truncate_file, self.seek_pointer)?;
         Ok(())
     }
 }
