749b43e4d93fa735f1f01df7d82cfd2d0b6a7c70
Dániel Buga <bugadani@gmail.com>
2024-06-02 19:24:32 +0200

    Implement nRF detection

--- probe-rs-target/src/chip_detection.rs
+++ probe-rs-target/src/chip_detection.rs
@@ -12,6 +12,12 @@ pub enum ChipDetectionMethod {
 
     /// Espressif chip detection information.
     Espressif(EspressifDetection),
+
+    /// Nordic Semiconductor FICR CONFIGID-based chip detection information.
+    NordicConfigId(NordicConfigIdDetection),
+
+    /// Nordic Semiconductor FICR INFO-based chip detection information.
+    NordicFicrInfo(NordicFicrDetection),
 }
 
 impl ChipDetectionMethod {
@@ -27,6 +33,24 @@ impl ChipDetectionMethod {
     /// Returns the Espressif detection information if available.
     pub fn as_espressif(&self) -> Option<&EspressifDetection> {
         if let Self::Espressif(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns the Nordic CONFIGID detection information if available.
+    pub fn as_nordic_configid(&self) -> Option<&NordicConfigIdDetection> {
+        if let Self::NordicConfigId(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns the Nordic FICR detection information if available.
+    pub fn as_nordic_ficr(&self) -> Option<&NordicFicrDetection> {
+        if let Self::NordicFicrInfo(v) = self {
             Some(v)
         } else {
             None
@@ -57,5 +81,31 @@ pub struct EspressifDetection {
     pub idcode: u32,
 
     /// Magic chip value => Target name.
+    pub variants: HashMap<u32, String>,
+}
+
+/// Nordic FICR CONFIGID-based chip detection information.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NordicConfigIdDetection {
+    /// FICR CONFIGID address
+    pub configid_address: u32,
+
+    /// CONFIGID.HWID => Target name.
+    pub hwid: HashMap<u32, String>,
+}
+
+/// Nordic FICR INFO-based chip detection information.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NordicFicrDetection {
+    /// FICR INFO.PART address
+    pub part_address: u32,
+
+    /// FICR INFO.VARIANT address
+    pub variant_address: u32,
+
+    /// The value of INFO.PART
+    pub part: u32,
+
+    /// INFO.VARIANT => Target name.
     pub variants: HashMap<u32, String>,
 }
