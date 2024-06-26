31cfa4d1e19efa865d3744da09a1aa326787d558
Dániel Buga <bugadani@gmail.com>
2024-06-26 14:46:33 +0200

    Validate by constructing Targets

--- probe-rs/src/config/registry.rs
+++ probe-rs/src/config/registry.rs
@@ -497,7 +497,13 @@ mod tests {
         registry
             .families
             .iter()
-            .map(|family| family.validate())
+            .flat_map(|family| {
+                // Validate all chip descriptors by constructing a Target.
+                family
+                    .variants()
+                    .iter()
+                    .map(|chip| registry.get_target(family, chip))
+            })
             .collect::<Result<Vec<_>, _>>()
             .unwrap();
     }
