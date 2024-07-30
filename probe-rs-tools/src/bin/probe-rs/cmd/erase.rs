4025d37dec1b955b7285026ee0bda0b26c257aca
Dániel Buga <bugadani@gmail.com>
2024-07-30 19:01:06 +0200

    Handle erase progress events

--- probe-rs-tools/src/bin/probe-rs/cmd/erase.rs
+++ probe-rs-tools/src/bin/probe-rs/cmd/erase.rs
@@ -1,9 +1,12 @@
+use std::cell::RefCell;
+
+use indicatif::{MultiProgress, ProgressBar};
 use probe_rs::{
-    flashing::{erase_all, FlashProgress},
+    flashing::{erase_all, FlashProgress, ProgressEvent},
     probe::list::Lister,
 };
 
-use crate::util::common_options::ProbeOptions;
+use crate::util::{common_options::ProbeOptions, flash::ProgressBarGroup, logging};
 
 #[derive(clap::Parser)]
 pub struct Cmd {
@@ -15,7 +18,43 @@ impl Cmd {
     pub fn run(self, lister: &Lister) -> anyhow::Result<()> {
         let (mut session, _probe_options) = self.common.simple_attach(lister)?;
 
-        erase_all(&mut session, FlashProgress::empty())?;
+        let multi_progress = MultiProgress::new();
+        logging::set_progress_bar(multi_progress.clone());
+
+        let progress_bars = RefCell::new(ProgressBarGroup::new("Erasing"));
+
+        let progress = FlashProgress::new(move |event| {
+            let mut progress_bars = progress_bars.borrow_mut();
+
+            match event {
+                ProgressEvent::Initialized {
+                    chip_erase, phases, ..
+                } => {
+                    // Build progress bars.
+                    if chip_erase {
+                        progress_bars.add(multi_progress.add(ProgressBar::new(0)));
+                    }
+
+                    if !chip_erase {
+                        if phases.len() > 1 {
+                            progress_bars.append_phase();
+                        }
+
+                        for phase_layout in phases {
+                            let sector_size =
+                                phase_layout.sectors().iter().map(|s| s.size()).sum::<u64>();
+                            progress_bars.add(multi_progress.add(ProgressBar::new(sector_size)));
+                        }
+                    }
+                }
+                ProgressEvent::SectorErased { size, .. } => progress_bars.inc(size),
+                ProgressEvent::FailedErasing => progress_bars.abandon(),
+                ProgressEvent::FinishedErasing => progress_bars.finish(),
+                _ => {}
+            }
+        });
+
+        erase_all(&mut session, progress)?;
 
         Ok(())
     }
