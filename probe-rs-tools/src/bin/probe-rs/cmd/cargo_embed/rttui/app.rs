e9243e27f0d20653facf46e1962d7b9a3f5845fe
Dániel Buga <bugadani@gmail.com>
2024-08-08 16:34:54 +0200

    Remove RttActiveDOwnChannel::channl_name

--- probe-rs-tools/src/bin/probe-rs/cmd/cargo_embed/rttui/app.rs
+++ probe-rs-tools/src/bin/probe-rs/cmd/cargo_embed/rttui/app.rs
@@ -88,7 +88,7 @@ impl App {
                         0
                     },
                     down_channel: Some(number),
-                    name: Some(down.channel_name.clone()),
+                    name: Some(down.channel_name()),
                     hide: false,
                 });
             }
