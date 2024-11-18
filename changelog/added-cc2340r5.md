81fb228421f7c486319c1dfc953c4a73c3237951
Sean Lyons <seanmlyons22@gmail.com>
2024-11-18 19:53:48 +0100

    Initial support for the TI cc23xx_cc27xx device family
     * Add support for CC2340R5 as starting point
    
    Ref: https://www.ti.com/lit/ug/swcu193/swcu193.pdf

---
+++ changelog/added-cc2340r5.md
@@ -0,0 +1,1 @@
+Added support for TI's CC2340R5.
--- probe-rs/src/vendor/ti/mod.rs
+++ probe-rs/src/vendor/ti/mod.rs
@@ -4,7 +4,10 @@ use probe_rs_target::Chip;
 
 use crate::{
     config::DebugSequence,
-    vendor::{Vendor, ti::sequences::cc13xx_cc26xx::CC13xxCC26xx},
+    vendor::{
+        ti::sequences::{cc13xx_cc26xx::CC13xxCC26xx, cc23xx_cc27xx::CC23xxCC27xx},
+        Vendor,
+    },
 };
 
 pub mod sequences;
@@ -17,6 +20,8 @@ impl Vendor for TexasInstruments {
     fn try_create_debug_sequence(&self, chip: &Chip) -> Option<DebugSequence> {
         let sequence = if chip.name.starts_with("CC13") || chip.name.starts_with("CC26") {
             DebugSequence::Arm(CC13xxCC26xx::create(chip.name.clone()))
+        } else if chip.name.starts_with("CC23") || chip.name.starts_with("CC27") {
+            DebugSequence::Arm(CC23xxCC27xx::create(chip.name.clone()))
         } else {
             return None;
         };
