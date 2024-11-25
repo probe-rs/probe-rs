9a569b7aa1c35029864612f54663096232b2470d
Robin Mueller <robin.mueller.m@gmail.com>
2024-11-25 17:22:56 +0100

    only select TAP if scan chain is available

--- probe-rs/src/session.rs
+++ probe-rs/src/session.rs
@@ -207,8 +207,10 @@ impl Session {
             }
         }
         probe.attach_to_unspecified()?;
-        for core in &cores {
-            probe.select_jtag_tap(core.interface_idx())?;
+        if probe.scan_chain().iter().len() > 0 {
+            for core in &cores {
+                probe.select_jtag_tap(core.interface_idx())?;
+            }
         }
 
         let interface = probe.try_into_arm_interface().map_err(|(_, err)| err)?;
