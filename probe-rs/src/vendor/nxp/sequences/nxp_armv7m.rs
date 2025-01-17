6148b8f16f9e1e50d9d81d0bb8938c5bbe653281
Ian McIntyre <me@mciantyre.dev>
2025-01-17 14:59:34 -0500

    Halt the MIMXRT11xx before changing FlexRAM (#3020)

--- probe-rs/src/vendor/nxp/sequences/nxp_armv7m.rs
+++ probe-rs/src/vendor/nxp/sequences/nxp_armv7m.rs
@@ -416,6 +416,7 @@ impl ArmDebugSequence for MIMXRT117x {
     ) -> Result<(), ArmError> {
         // OK to perform before the reset, since the configuration
         // persists beyond the reset.
+        self.halt(probe, true)?;
         self.use_boot_fuses_for_flexram(probe)?;
 
         // Cache debug system state that may be lost across the reset.
