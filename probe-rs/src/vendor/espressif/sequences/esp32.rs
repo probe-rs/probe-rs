50639e079ff313eabfc2d2f48ce11fc15f338b61
Dániel Buga <bugadani@gmail.com>
2025-03-17 21:50:10 +0100

    Disable WDTs every time MCU is halted (#3171)

--- probe-rs/src/vendor/espressif/sequences/esp32.rs
+++ probe-rs/src/vendor/espressif/sequences/esp32.rs
@@ -37,39 +37,51 @@ impl ESP32 {
             },
         })
     }
-}
 
-impl XtensaDebugSequence for ESP32 {
-    fn on_connect(&self, core: &mut XtensaCommunicationInterface) -> Result<(), crate::Error> {
-        // Peripheral address range
-        core.add_slow_memory_access_range(0x3FF0_0000..0x3FF8_0000);
+    fn disable_wdts(
+        &self,
+        interface: &mut XtensaCommunicationInterface,
+    ) -> Result<(), crate::Error> {
         tracing::info!("Disabling ESP32 watchdogs...");
 
         // tg0 wdg
         const TIMG0_BASE: u64 = 0x3ff5f000;
         const TIMG0_WRITE_PROT: u64 = TIMG0_BASE | 0x64;
         const TIMG0_WDTCONFIG0: u64 = TIMG0_BASE | 0x48;
-        core.write_word_32(TIMG0_WRITE_PROT, 0x50D83AA1)?; // write protection off
-        core.write_word_32(TIMG0_WDTCONFIG0, 0x0)?;
-        core.write_word_32(TIMG0_WRITE_PROT, 0x0)?; // write protection on
+        interface.write_word_32(TIMG0_WRITE_PROT, 0x50D83AA1)?; // write protection off
+        interface.write_word_32(TIMG0_WDTCONFIG0, 0x0)?;
+        interface.write_word_32(TIMG0_WRITE_PROT, 0x0)?; // write protection on
 
         // tg1 wdg
         const TIMG1_BASE: u64 = 0x3ff60000;
         const TIMG1_WRITE_PROT: u64 = TIMG1_BASE | 0x64;
         const TIMG1_WDTCONFIG0: u64 = TIMG1_BASE | 0x48;
-        core.write_word_32(TIMG1_WRITE_PROT, 0x50D83AA1)?; // write protection off
-        core.write_word_32(TIMG1_WDTCONFIG0, 0x0)?;
-        core.write_word_32(TIMG1_WRITE_PROT, 0x0)?; // write protection on
+        interface.write_word_32(TIMG1_WRITE_PROT, 0x50D83AA1)?; // write protection off
+        interface.write_word_32(TIMG1_WDTCONFIG0, 0x0)?;
+        interface.write_word_32(TIMG1_WRITE_PROT, 0x0)?; // write protection on
 
         // rtc wdg
         const RTC_CNTL_BASE: u64 = 0x3ff48000;
         const RTC_WRITE_PROT: u64 = RTC_CNTL_BASE | 0xa4;
         const RTC_WDTCONFIG0: u64 = RTC_CNTL_BASE | 0x8c;
-        core.write_word_32(RTC_WRITE_PROT, 0x50D83AA1)?; // write protection off
-        core.write_word_32(RTC_WDTCONFIG0, 0x0)?;
-        core.write_word_32(RTC_WRITE_PROT, 0x0)?; // write protection on
+        interface.write_word_32(RTC_WRITE_PROT, 0x50D83AA1)?; // write protection off
+        interface.write_word_32(RTC_WDTCONFIG0, 0x0)?;
+        interface.write_word_32(RTC_WRITE_PROT, 0x0)?; // write protection on
 
         Ok(())
+    }
+}
+
+impl XtensaDebugSequence for ESP32 {
+    fn on_connect(&self, interface: &mut XtensaCommunicationInterface) -> Result<(), crate::Error> {
+        // Peripheral address range
+        interface.add_slow_memory_access_range(0x3FF0_0000..0x3FF8_0000);
+
+        self.disable_wdts(interface)
+    }
+
+    fn on_halt(&self, interface: &mut XtensaCommunicationInterface) -> Result<(), crate::Error> {
+        self.disable_wdts(interface)
     }
 
     fn detect_flash_size(&self, session: &mut Session) -> Result<Option<usize>, crate::Error> {
