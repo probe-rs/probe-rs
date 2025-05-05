71ed92ce4aa8ed8be8a4c1d33396ca9a5ba8f7c2
Dániel Buga <bugadani@gmail.com>
2025-05-05 16:23:03 +0200

    Outline default impsl

--- probe-rs-debug/src/exception_handling/riscv.rs
+++ probe-rs-debug/src/exception_handling/riscv.rs
@@ -1,8 +1,7 @@
 use std::ops::ControlFlow;
 
 use crate::{
-    DebugError, DebugInfo, DebugRegisters, StackFrame,
-    exception_handling::{ExceptionInfo, ExceptionInterface},
+    DebugError, DebugRegisters, StackFrame, exception_handling::ExceptionInterface,
     unwind_pc_without_debuginfo,
 };
 
@@ -11,42 +10,6 @@ use probe_rs::{MemoryInterface, RegisterRole, RegisterValue};
 pub struct RiscvExceptionHandler;
 
 impl ExceptionInterface for RiscvExceptionHandler {
-    fn exception_details(
-        &self,
-        _memory: &mut dyn MemoryInterface,
-        _stackframe_registers: &DebugRegisters,
-        _debug_info: &DebugInfo,
-    ) -> Result<Option<ExceptionInfo>, DebugError> {
-        // For architectures where the exception handling has not been implemented in probe-rs,
-        // this will result in maintaining the current `unwind` behavior, i.e. unwinding will include up
-        // to the first frame that was called from an exception handler.
-        Ok(None)
-    }
-
-    fn calling_frame_registers(
-        &self,
-        _memory: &mut dyn MemoryInterface,
-        _stackframe_registers: &crate::DebugRegisters,
-        _raw_exception: u32,
-    ) -> Result<crate::DebugRegisters, DebugError> {
-        Err(DebugError::NotImplemented("calling frame registers"))
-    }
-
-    fn raw_exception(
-        &self,
-        _stackframe_registers: &crate::DebugRegisters,
-    ) -> Result<u32, DebugError> {
-        Err(DebugError::NotImplemented("raw exception"))
-    }
-
-    fn exception_description(
-        &self,
-        _raw_exception: u32,
-        _memory: &mut dyn MemoryInterface,
-    ) -> Result<String, DebugError> {
-        Err(DebugError::NotImplemented("exception description"))
-    }
-
     fn unwind_without_debuginfo(
         &self,
         unwind_registers: &mut DebugRegisters,
