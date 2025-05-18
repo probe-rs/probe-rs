7b42b89bfa1cf5aefb69a882e894af5a90e19d8e
Sean Cross <sean@xobs.io>
2025-05-18 20:28:34 +0800

    probe-rs: add blank-check to flash algorithm
    Some parts have special requirements for determining whether it is blank.
    Add functions to perform this operation.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- probe-rs/src/flashing/erase.rs
+++ probe-rs/src/flashing/erase.rs
@@ -247,3 +247,90 @@ pub fn erase_sectors(
 
     Ok(())
 }
+
+/// Check that a memory range has been erased.
+pub fn run_blank_check(
+    session: &mut Session,
+    progress: FlashProgress,
+    start_sector: usize,
+    sectors: usize,
+) -> Result<(), FlashError> {
+    tracing::debug!("Performing blank check...");
+
+    let mut algos: HashMap<(String, String), Vec<NvmRegion>> = HashMap::new();
+    tracing::debug!("Regions:");
+    for region in session
+        .target()
+        .memory_map
+        .iter()
+        .filter_map(MemoryRegion::as_nvm_region)
+    {
+        if region.is_alias {
+            tracing::debug!("Skipping alias memory region {:#010x?}", region.range);
+            continue;
+        }
+        tracing::debug!(
+            "    region: {:#010x?} ({} bytes)",
+            region.range,
+            region.range.end - region.range.start
+        );
+
+        let algo = FlashLoader::get_flash_algorithm_for_region(region, session.target())?;
+
+        // Get the first core that can access the region
+        let core_name = region
+            .cores
+            .first()
+            .ok_or_else(|| FlashError::NoNvmCoreAccess(region.clone()))?;
+
+        let entry = algos
+            .entry((algo.name.clone(), core_name.clone()))
+            .or_default();
+        entry.push(region.clone());
+
+        tracing::debug!("     -- using algorithm: {}", algo.name);
+    }
+
+    for ((algo_name, core_name), regions) in algos {
+        tracing::debug!("Checking for blank sector with algorithm: {}", algo_name);
+
+        // This can't fail, algo_name comes from the target.
+        let algo = session.target().flash_algorithm_by_name(&algo_name);
+        let algo = algo.unwrap();
+
+        let core_index = session.target().core_index_by_name(&core_name).unwrap();
+        let mut flasher = Flasher::new(session.target(), core_index, algo)?;
+
+        let sectors = flasher
+            .flash_algorithm()
+            .iter_sectors()
+            .skip(start_sector)
+            .take(sectors)
+            .filter(|info| {
+                let range = info.base_address..info.base_address + info.size;
+                regions.iter().any(|r| r.range.contains_range(&range))
+            })
+            .collect::<Vec<_>>();
+
+        flasher.run_blank_check(session, &progress, |active, _| {
+            for info in sectors {
+                tracing::debug!(
+                    "    sector: {:#010x}-{:#010x} ({} bytes)",
+                    info.base_address,
+                    info.base_address + info.size,
+                    info.size
+                );
+
+                let sector = FlashSector {
+                    address: info.base_address,
+                    size: info.size,
+                };
+
+                active.blank_check(&sector)?;
+            }
+            Ok(())
+        })?;
+    }
+
+    Ok(())
+}
