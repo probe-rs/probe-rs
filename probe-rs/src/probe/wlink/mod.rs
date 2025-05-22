probe-rs/probe-rs

commit ff8cace3d4e57dc0bf68f9bd453b8ee7ddf28f71

BitVec: the sequel (#3236)

probe-rs/src/probe/wlink/mod.rs
@@ -6,6 +6,7 @@
 use std::fmt;
 use std::time::Duration;
 
+use bitvec::{bitvec, order::Lsb0, vec::BitVec, view::BitView};
 use nusb::DeviceInfo;
 use probe_rs_target::ScanChainElement;
 
@@ -395,7 +396,7 @@ impl JTAGAccess for WchLink {
         Ok(())
     }
 
-    fn read_register(&mut self, address: u32, len: u32) -> Result<Vec<u8>, DebugProbeError> {
+    fn read_register(&mut self, address: u32, len: u32) -> Result<BitVec, DebugProbeError> {
         tracing::debug!("read register 0x{:08x}", address);
         assert_eq!(len, 32);
 
@@ -403,14 +404,14 @@ impl JTAGAccess for WchLink {
             REG_IDCODE_ADDRESS => {
                 // using hard coded idcode 0x00000001, the same as WCH's openocd fork
                 tracing::debug!("using hard coded idcode 0x00000001");
-                Ok(0x1_u32.to_le_bytes().to_vec())
+                Ok(0x1_usize.view_bits().to_bitvec())
             }
             REG_DTMCS_ADDRESS => {
                 // See: RISC-V Debug Specification, 6.1.4
                 // 0x71: abits=7, version=1(1.0)
-                Ok(0x71_u32.to_le_bytes().to_vec())
+                Ok(0x71_usize.view_bits().to_bitvec())
             }
-            REG_BYPASS_ADDRESS => Ok(vec![0; 4]),
+            REG_BYPASS_ADDRESS => Ok(bitvec![0; 4]),
             _ => panic!("unknown read register address {address:08x}"),
         }
     }
@@ -429,7 +430,7 @@ impl JTAGAccess for WchLink {
         address: u32,
         data: &[u8],
         len: u32,
-    ) -> Result<Vec<u8>, DebugProbeError> {
+    ) -> Result<BitVec, DebugProbeError> {
         match address as u8 {
             REG_DTMCS_ADDRESS => {
                 let val = u32::from_le_bytes(data.try_into().unwrap());
@@ -442,7 +443,7 @@ impl JTAGAccess for WchLink {
                     return Err(WchLinkError::UnsupportedOperation.into());
                 }
 
-                Ok(0x71_u32.to_le_bytes().to_vec())
+                Ok(0x71_usize.view_bits().to_bitvec())
             }
             REG_DMI_ADDRESS => {
                 assert_eq!(
@@ -462,15 +463,12 @@ impl JTAGAccess for WchLink {
                     dmi_value,
                 );
 
-                match dmi_op {
+                let (addr, data, op) = match dmi_op {
                     DMI_OP_READ => {
                         let (addr, data, op) = self.dmi_op_read(dmi_addr)?;
-                        let ret = ((addr as u128) << DMI_ADDRESS_BIT_OFFSET)
-                            | ((data as u128) << DMI_VALUE_BIT_OFFSET)
-                            | (op as u128);
                         tracing::trace!("dmi read 0x{:02x} 0x{:08x} op={}", addr, data, op);
                         self.last_dmi_read = Some((addr, data, op));
-                        Ok(ret.to_le_bytes().to_vec())
+                        (addr, data, op)
                     }
                     DMI_OP_NOP => {
                         // No idea why NOP with zero addr should return the last read value.
@@ -480,33 +478,38 @@ impl JTAGAccess for WchLink {
                         } else {
                             self.dmi_op_nop()?
                         };
-
-                        let ret = ((addr as u128) << DMI_ADDRESS_BIT_OFFSET)
-                            | ((data as u128) << DMI_VALUE_BIT_OFFSET)
-                            | (op as u128);
                         tracing::trace!("dmi nop 0x{:02x} 0x{:08x} op={}", addr, data, op);
-                        Ok(ret.to_le_bytes().to_vec())
+                        (addr, data, op)
                     }
                     DMI_OP_WRITE => {
                         let (addr, data, op) = self.dmi_op_write(dmi_addr, dmi_value)?;
-                        let ret = ((addr as u128) << DMI_ADDRESS_BIT_OFFSET)
-                            | ((data as u128) << DMI_VALUE_BIT_OFFSET)
-                            | (op as u128);
                         tracing::trace!("dmi write 0x{:02x} 0x{:08x} op={}", addr, data, op);
                         if dmi_addr == 0x10 && dmi_value == 0x40000001 {
                             // needs additional sleep for a resume operation
                             std::thread::sleep(Duration::from_millis(10));
                         }
-                        Ok(ret.to_le_bytes().to_vec())
+                        (addr, data, op)
                     }
                     _ => unreachable!("unknown dmi_op {dmi_op}"),
+                };
+
+                let ret = ((addr as u128) << DMI_ADDRESS_BIT_OFFSET)
+                    | ((data as u128) << DMI_VALUE_BIT_OFFSET)
+                    | (op as u128);
+
+                let ret_bytes = ret.to_le_bytes();
+                let mut ret = bitvec![0;32];
+                for byte in ret_bytes.iter() {
+                    ret.extend_from_bitslice(byte.view_bits::<Lsb0>());
                 }
+
+                Ok(ret)
             }
             _ => unreachable!("unknown register address 0x{:08x}", address),
         }
     }
 
-    fn write_dr(&mut self, _data: &[u8], _len: u32) -> Result<Vec<u8>, DebugProbeError> {
+    fn write_dr(&mut self, _data: &[u8], _len: u32) -> Result<BitVec, DebugProbeError> {
         Err(DebugProbeError::NotImplemented {
             function_name: "write_dr",
         })

probe-rs/src/probe/blackmagic/mod.rs
@@ -610,7 +610,7 @@ pub struct BlackMagicProbe {
     jtag_state: JtagDriverState,
     probe_statistics: ProbeStatistics,
     swd_settings: SwdSettings,
-    in_bits: BitVec<u8>,
+    in_bits: BitVec,
     swd_direction: SwdDirection,
 }
 
@@ -1374,7 +1374,7 @@ impl RawJtagIo for BlackMagicProbe {
         Ok(())
     }
 
-    fn read_captured_bits(&mut self) -> Result<BitVec<u8>, DebugProbeError> {
+    fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
         tracing::trace!("reading captured bits");
         Ok(std::mem::take(&mut self.in_bits))
     }

probe-rs/src/probe/cmsisdap/commands/jtag/mod.rs
@@ -29,7 +29,7 @@ impl RawJtagIo for CmsisDap {
         Ok(())
     }
 
-    fn read_captured_bits(&mut self) -> Result<BitVec<u8>, DebugProbeError> {
+    fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
         self.flush_jtag()?;
         Ok(std::mem::take(&mut self.jtag_buffer.response))
     }
@@ -112,7 +112,7 @@ pub(crate) struct JtagBuffer {
     packet_size: usize,
     current_sequence: Option<JtagSequence>,
     complete_sequences: Vec<JtagSequence>,
-    response: BitVec<u8>,
+    response: BitVec,
 }
 impl JtagBuffer {
     pub(crate) fn new(packet_size: u16) -> Self {

probe-rs/src/probe/espusbjtag/mod.rs
@@ -70,7 +70,7 @@ impl RawJtagIo for EspUsbJtag {
         Ok(())
     }
 
-    fn read_captured_bits(&mut self) -> Result<BitVec<u8>, DebugProbeError> {
+    fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
         self.protocol.read_captured_bits()
     }
 

probe-rs/src/probe/ftdi/mod.rs
@@ -39,7 +39,7 @@ struct JtagAdapter {
     command: Command,
     commands: Vec<u8>,
     in_bit_counts: Vec<usize>,
-    in_bits: BitVec<u8>,
+    in_bits: BitVec,
     ftdi: FtdiProperties,
 }
 
@@ -249,7 +249,7 @@ impl JtagAdapter {
         Ok(())
     }
 
-    fn read_captured_bits(&mut self) -> Result<BitVec<u8>, DebugProbeError> {
+    fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
         self.flush()?;
 
         Ok(std::mem::take(&mut self.in_bits))
@@ -499,7 +499,7 @@ impl RawJtagIo for FtdiProbe {
         Ok(())
     }
 
-    fn read_captured_bits(&mut self) -> Result<BitVec<u8>, DebugProbeError> {
+    fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
         self.adapter.read_captured_bits()
     }
 

probe-rs/src/probe/jlink/mod.rs
@@ -385,7 +385,7 @@ pub struct JLink {
     jtag_tms_bits: Vec<bool>,
     jtag_tdi_bits: Vec<bool>,
     jtag_capture_tdo: Vec<bool>,
-    jtag_response: BitVec<u8>,
+    jtag_response: BitVec,
     jtag_state: JtagDriverState,
 
     /// max number of bits in a transfer chunk, when using JTAG
@@ -781,7 +781,7 @@ impl JLink {
         Ok(())
     }
 
-    fn read_captured_bits(&mut self) -> Result<BitVec<u8>, DebugProbeError> {
+    fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
         self.flush_jtag()?;
 
         Ok(std::mem::take(&mut self.jtag_response))
@@ -1271,7 +1271,7 @@ impl RawJtagIo for JLink {
         self.shift_jtag_bit(tms, tdi, capture)
     }
 
-    fn read_captured_bits(&mut self) -> Result<BitVec<u8>, DebugProbeError> {
+    fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
         self.read_captured_bits()
     }
 }

probe-rs/src/architecture/riscv/dtm/jtag_dtm.rs
@@ -4,6 +4,8 @@
 //! Currently, only JTAG is supported.
 
 use bitfield::bitfield;
+use bitvec::field::BitField;
+use bitvec::slice::BitSlice;
 use std::time::{Duration, Instant};
 
 use crate::architecture::riscv::communication_interface::{
@@ -90,8 +92,8 @@ impl<'probe> JtagDtm<'probe> {
         Self { probe, state }
     }
 
-    fn transform_dmi_result(response_bytes: Vec<u8>) -> Result<u32, DmiOperationStatus> {
-        let response_value = u128_from_data(&response_bytes);
+    fn transform_dmi_result(response_bits: &BitSlice) -> Result<u32, DmiOperationStatus> {
+        let response_value = response_bits.load_le::<u128>();
 
         // Verify that the transfer was ok
         let op = (response_value & DMI_OP_MASK) as u8;
@@ -118,7 +120,7 @@ impl<'probe> JtagDtm<'probe> {
 
         self.probe
             .write_register(DMI_ADDRESS, &bytes, bit_size)
-            .map(Self::transform_dmi_result)
+            .map(|bits| Self::transform_dmi_result(&bits))
     }
 
     fn schedule_dmi_register_access(
@@ -174,7 +176,7 @@ impl DtmAccess for JtagDtm<'_> {
         self.probe.tap_reset()?;
         let dtmcs_raw = self.probe.read_register(DTMCS_ADDRESS, DTMCS_WIDTH)?;
 
-        let raw_dtmcs = u32::from_le_bytes((&dtmcs_raw[..]).try_into().unwrap());
+        let raw_dtmcs = dtmcs_raw.load_le::<u32>();
 
         if raw_dtmcs == 0 {
             return Err(RiscvError::NoRiscvTarget);
@@ -307,7 +309,7 @@ impl DtmAccess for JtagDtm<'_> {
     fn read_idcode(&mut self) -> Result<Option<u32>, DebugProbeError> {
         let value = self.probe.read_register(0x1, 32)?;
 
-        Ok(Some(u32::from_le_bytes((&value[..]).try_into().unwrap())))
+        Ok(Some(value.load_le::<u32>()))
     }
 }
 
@@ -355,7 +357,7 @@ impl<'probe> TunneledJtagDtm<'probe> {
         let result = self
             .probe
             .write_dr(&cmd.data, cmd.len)
-            .map(|r| (cmd.transform)(&cmd, r))?;
+            .map(|r| (cmd.transform)(&cmd, &r))?;
         match result {
             Ok(CommandResult::U32(d)) => Ok(d),
             Err(crate::Error::Probe(e)) => Err(e.into()),
@@ -363,10 +365,8 @@ impl<'probe> TunneledJtagDtm<'probe> {
         }
     }
 
-    fn transform_tunneled_dr_result(response_bytes: Vec<u8>) -> Vec<u8> {
-        let raw_response = u128_from_data(&response_bytes);
-        let response = raw_response >> 4;
-        response.to_le_bytes().into()
+    fn transform_tunneled_dr_result(response_bits: &BitSlice) -> &BitSlice {
+        &response_bits[4..]
     }
 
     fn dmi_register_access(
@@ -381,10 +381,9 @@ impl<'probe> TunneledJtagDtm<'probe> {
 
         let dmi_bits = self.state.abits + DMI_ADDRESS_BIT_OFFSET;
         let (bit_size, bytes) = op.to_tunneled_byte_batch(dmi_bits);
-        self.probe
-            .write_dr(&bytes, bit_size)
-            .map(Self::transform_tunneled_dr_result)
-            .map(JtagDtm::transform_dmi_result)
+        let result = self.probe.write_dr(&bytes, bit_size)?;
+        let tunneled_result = Self::transform_tunneled_dr_result(&result);
+        Ok(JtagDtm::transform_dmi_result(tunneled_result))
     }
 
     fn schedule_dmi_register_access(
@@ -568,7 +567,7 @@ impl DtmAccess for TunneledJtagDtm<'_> {
 
     fn read_idcode(&mut self) -> Result<Option<u32>, DebugProbeError> {
         let value = self.probe.read_register(0x1, 32)?;
-        Ok(Some(u32::from_le_bytes((&value[..]).try_into().unwrap())))
+        Ok(Some(value.load_le::<u32>()))
     }
 }
 
@@ -596,8 +595,7 @@ fn tunnel_dtmcs_command(data: u32) -> ShiftDrCommand {
         data: tunneled_dr.to_le_bytes().into(),
         len: (msb_offset as u32) + 1,
         transform: |_, result| {
-            let raw_response = u128_from_data(&result);
-            let response = (raw_response >> 4) as u32;
+            let response = result[4..].load_le::<u32>();
             Ok(CommandResult::U32(response))
         },
     }
@@ -677,13 +675,6 @@ impl DmiOperationStatus {
 
         Some(status)
     }
-}
-
-fn u128_from_data(data: &[u8]) -> u128 {
-    data.iter().enumerate().fold(0, |acc, elem| {
-        let (byte_offset, value) = elem;
-        acc + ((*value as u128) << (8 * byte_offset))
-    })
 }
 
 /// Address of the `dtmcs` JTAG register.

probe-rs/src/architecture/xtensa/xdm.rs
@@ -3,6 +3,8 @@ use std::{
     time::{Duration, Instant},
 };
 
+use bitvec::{field::BitField, slice::BitSlice};
+
 use crate::{
     Error as ProbeRsError,
     architecture::xtensa::arch::instruction::{Instruction, InstructionEncoding},
@@ -52,19 +54,6 @@ impl TapInstruction {
             TapInstruction::PowerControl => 8,
             TapInstruction::PowerStatus => 8,
             TapInstruction::Idcode => 32,
-        }
-    }
-
-    fn capture_to_u8(self, capture: &[u8]) -> u8 {
-        capture[0]
-    }
-
-    fn capture_to_u32(self, capture: &[u8]) -> u32 {
-        match self {
-            TapInstruction::Ndr | TapInstruction::Idcode => {
-                u32::from_le_bytes(capture.try_into().unwrap())
-            }
-            _ => capture[0] as u32,
         }
     }
 }
@@ -380,7 +369,7 @@ impl<'probe> Xdm<'probe> {
             data: nar.to_le_bytes().to_vec(),
             len: TapInstruction::Nar.bits(),
             transform: |write, capture| {
-                let capture = TapInstruction::Nar.capture_to_u8(&capture);
+                let capture = capture.load_le::<u8>();
                 let nar = write.data[0] >> 1;
                 let write = write.data[0] & 1 == 1;
 
@@ -438,8 +427,7 @@ impl<'probe> Xdm<'probe> {
             .probe
             .write_register(instr.code(), &[value], instr.bits())?;
 
-        let res = instr.capture_to_u8(&capture);
-
+        let res = capture.load_le::<u8>();
         tracing::trace!("pwr_write response: {:?}", res);
 
         Ok(res)
@@ -452,7 +440,7 @@ impl<'probe> Xdm<'probe> {
             .probe
             .write_register(instr.code(), &[0, 0, 0, 0], instr.bits())?;
 
-        let res = instr.capture_to_u32(&capture);
+        let res = capture.load_le::<u32>();
 
         tracing::debug!("idcode response: {:x?}", res);
 
@@ -692,29 +680,27 @@ impl<'probe> Xdm<'probe> {
     }
 }
 
-type TransformFn = fn(&ShiftDrCommand, Vec<u8>) -> Result<CommandResult, ProbeRsError>;
+type TransformFn = fn(&ShiftDrCommand, &BitSlice) -> Result<CommandResult, ProbeRsError>;
 
 fn transform_u32(
     _command: &ShiftDrCommand,
-    capture: Vec<u8>,
+    capture: &BitSlice,
 ) -> Result<CommandResult, ProbeRsError> {
-    Ok(CommandResult::U32(
-        TapInstruction::Ndr.capture_to_u32(&capture),
-    ))
+    Ok(CommandResult::U32(capture.load_le::<u32>()))
 }
 
 fn transform_noop(
     _command: &ShiftDrCommand,
-    _capture: Vec<u8>,
+    _capture: &BitSlice,
 ) -> Result<CommandResult, ProbeRsError> {
     Ok(CommandResult::None)
 }
 
 fn transform_instruction_status(
     _command: &ShiftDrCommand,
-    capture: Vec<u8>,
+    capture: &BitSlice,
 ) -> Result<CommandResult, ProbeRsError> {
-    let status = DebugStatus(TapInstruction::Ndr.capture_to_u32(&capture));
+    let status = DebugStatus(capture.load_le::<u32>());
 
     if status.exec_overrun() {
         return Err(ProbeRsError::Xtensa(XtensaError::XdmError(

commit f2e4941fa003fc14eeb733af813a5e26e7ab7434

fix: Have wlink return proper bitvec sizes
This fixes a regression introduced by
ff8cace3d4e57dc0bf68f9bd453b8ee7ddf28f71 BitVec: the sequel (#3236)

The following panic is shown using a wlink probe on an amd64 system.

thread 'tokio-runtime-worker' panicked at /home/arthur/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitvec-1.0.1/src/field.rs:526:2:
cannot load 32 bits from a 64-bit region

Return the requested bitvec size rather than whathever usize is.

Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

Add changelog

Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

probe-rs/src/probe/wlink/mod.rs
@@ -6,7 +6,7 @@
 use std::fmt;
 use std::time::Duration;
 
-use bitvec::{bitvec, order::Lsb0, vec::BitVec, view::BitView};
+use bitvec::{bitvec, field::BitField, order::Lsb0, vec::BitVec, view::BitView};
 use nusb::DeviceInfo;
 use probe_rs_target::ScanChainElement;
 
@@ -393,16 +393,19 @@ impl JtagAccess for WchLink {
         tracing::debug!("read register 0x{:08x}", address);
         assert_eq!(len, 32);
 
+        let mut ret = bitvec![0; len as usize];
         match address as u8 {
             REG_IDCODE_ADDRESS => {
                 // using hard coded idcode 0x00000001, the same as WCH's openocd fork
                 tracing::debug!("using hard coded idcode 0x00000001");
-                Ok(0x1_usize.view_bits().to_bitvec())
+                ret[0..8].store_le::<u8>(0x1);
+                Ok(ret)
             }
             REG_DTMCS_ADDRESS => {
                 // See: RISC-V Debug Specification, 6.1.4
                 // 0x71: abits=7, version=1(1.0)
-                Ok(0x71_usize.view_bits().to_bitvec())
+                ret[0..8].store_le::<u8>(0x71);
+                Ok(ret)
             }
             REG_BYPASS_ADDRESS => Ok(bitvec![0; 4]),
             _ => panic!("unknown read register address {address:08x}"),
@@ -436,7 +439,9 @@ impl JtagAccess for WchLink {
                     return Err(WchLinkError::UnsupportedOperation.into());
                 }
 
-                Ok(0x71_usize.view_bits().to_bitvec())
+                let mut ret = bitvec![0; len as usize];
+                ret[0..8].store_le::<u8>(0x71);
+                Ok(ret)
             }
             REG_DMI_ADDRESS => {
                 assert_eq!(
@@ -491,12 +496,12 @@ impl JtagAccess for WchLink {
                     | (op as u128);
 
                 let ret_bytes = ret.to_le_bytes();
-                let mut ret = bitvec![0;32];
-                for byte in ret_bytes.iter() {
-                    ret.extend_from_bitslice(byte.view_bits::<Lsb0>());
-                }
-
-                Ok(ret)
+                Ok(ret_bytes
+                    .iter()
+                    .fold(BitVec::with_capacity(128), |mut acc, s| {
+                        acc.extend_from_bitslice(s.view_bits::<Lsb0>());
+                        acc
+                    }))
             }
             _ => unreachable!("unknown register address 0x{:08x}", address),
         }