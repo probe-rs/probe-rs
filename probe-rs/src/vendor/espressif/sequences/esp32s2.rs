da47013dec177583e337a811d734d9873f59d94e
Dániel Buga <bugadani@gmail.com>
2025-09-11 10:25:34 +0200

    RISC-V: Configure memory access method based on address range (#3549)

--- probe-rs/src/vendor/espressif/sequences/esp32s2.rs
+++ probe-rs/src/vendor/espressif/sequences/esp32s2.rs
@@ -144,12 +144,11 @@ impl ESP32S2 {
 
         Ok(())
     }
-}
 
-impl XtensaDebugSequence for ESP32S2 {
-    fn on_connect(&self, interface: &mut XtensaCommunicationInterface) -> Result<(), crate::Error> {
-        self.disable_wdts(interface)?;
-
+    fn configure_memory_access(
+        &self,
+        interface: &mut XtensaCommunicationInterface<'_>,
+    ) -> Result<(), crate::Error> {
         // Internal Data Bus
         interface.core_properties().memory_ranges.insert(
             0x3FF9_E000..0x4000_0000,
@@ -169,6 +168,15 @@ impl XtensaDebugSequence for ESP32S2 {
             },
         );
         // External memory busses and peripheral address range uses the default (all false) properties.
+
+        Ok(())
+    }
+}
+
+impl XtensaDebugSequence for ESP32S2 {
+    fn on_connect(&self, interface: &mut XtensaCommunicationInterface) -> Result<(), crate::Error> {
+        self.configure_memory_access(interface)?;
+        self.disable_wdts(interface)?;
 
         Ok(())
     }
