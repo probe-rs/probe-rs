6e6c5faa5a05f1cd22eaab2866236b76de8fe7e0
Adam Greig <adam@adamgreig.com>
2026-01-03 19:48:41 +0000

    Flashing: Use A32-encoded BKPT on Armv7a

--- probe-rs/src/architecture/arm/assembly.rs
+++ probe-rs/src/architecture/arm/assembly.rs
@@ -1,4 +1,5 @@
 /// ARM breakpoint instruction (x2)
-pub const BKPT: u32 = 0xBE00_BE00;
+pub const BKPT_T32: u32 = 0xBE00_BE00;
+pub const BKPT_A32: u32 = 0xE1200070;
 /// ARM hlt instruction, Thumb2 (x2)
 pub const HLT: u32 = 0xBA80_BA80;
