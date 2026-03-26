e25cc8f5d1575d7e5a28332c095e6af08e15ae4e
Jonathan Keller <jonathan.keller@infineon.com>
2026-03-26 13:37:10 -0700

    simplify PSOC Edge debug sequences, add unit test to validate targets

--- probe-rs/src/vendor/infineon/sequences/psoc_edge.rs
+++ probe-rs/src/vendor/infineon/sequences/psoc_edge.rs
@@ -12,19 +12,61 @@ use crate::{
     config::CoreExt,
 };
 
+bitfield! {
+    /// CM55 control register.
+    #[derive(Clone, Copy)]
+    struct MxCm55Ctl(u32);
+    impl Debug;
+
+    /// Clock-gates the CM55, preventing it from starting until the CM33
+    /// has set up the system and initialized the CM55's vector table.
+    pub cm55_wait, set_cm55_wait: 4;
+}
+impl MxCm55Ctl {
+    const ADDRESS: u64 = 0x44160000;
+}
+
+bitfield! {
+    /// Application CPU subsystem access port control register.
+    #[derive(Clone, Copy)]
+    struct AppCpussApCtl(u32);
+    impl Debug;
+
+    /// Enables the CM55 debug access port.
+    pub cm55_enable, set_cm55_enable: 0;
+
+    /// Enables invasive debug access to the CM55 (halting, stepping, breakpooints).
+    pub cm55_dbg_enable, set_cm55_dbg_enable: 4;
+
+    /// Enables non-invasive debug access to the CM55 (tracing and memory access).
+    pub cm55_nid_enable, set_cm55_nid_enable: 5;
+}
+impl AppCpussApCtl {
+    const ADDRESS: u64 = 0x441C1000;
+}
+
 /// PSOC Edge debug sequences.
 #[derive(Debug)]
 pub struct PsocEdge {
-    /// The cores present on the device.
-    cores: Vec<probe_rs_target::Core>,
+    // The access port for the system CPU (CM33).
+    cm33_ap: FullyQualifiedApAddress,
+
+    // The access port for the application CPU (CM55).
+    cm55_ap: FullyQualifiedApAddress,
 }
 
 impl PsocEdge {
-    /// Tries to create debug sequences for a PSOC Edge chip.
+    /// Creates debug sequences for a PSOC Edge chip.
     pub fn create(chip: &Chip) -> Arc<Self> {
-        Arc::new(PsocEdge {
-            cores: chip.cores.clone(),
-        })
+        let [cm33, cm55] = &*chip.cores else {
+            unreachable!("PSOC Edge E84 devices have two cores");
+        };
+        let [cm33_ap, cm55_ap] = [cm33, cm55].map(|core| {
+            core.memory_ap()
+                .expect("PSOC Edge core must have a memory AP")
+        });
+
+        Arc::new(PsocEdge { cm33_ap, cm55_ap })
     }
 }
 
@@ -35,63 +77,14 @@ impl ArmDebugSequence for PsocEdge {
         core_ap: &FullyQualifiedApAddress,
         core_type: CoreType,
     ) -> Result<(), ArmError> {
-        bitfield! {
-            /// CM55 control register.
-            #[derive(Clone, Copy)]
-            struct MxCm55Ctl(u32);
-            impl Debug;
-
-            pub cm55_wait, set_cm55_wait: 4;
-        }
-        impl MxCm55Ctl {
-            const ADDRESS: u64 = 0x44160000;
-        }
-
-        #[derive(Debug, Clone, Copy)]
-        // Power Domain Dependency Sense register for power domain 6 (APPCPU).
-        struct AppCpuPdSense;
-        impl AppCpuPdSense {
-            const ADDRESS: u64 = 0x42410060;
-        }
-
-        // Power domain ID for the CM33.
-        const SYSCPU_PD: u32 = 4;
-        bitfield! {
-            /// CM55 access port control register.
-            #[derive(Clone, Copy)]
-            struct AppCpussApCtl(u32);
-            impl Debug;
-
-            pub cm55_enable, set_cm55_enable: 0;
-            pub cm55_dbg_enable, set_cm55_dbg_enable: 4;
-            pub cm55_nid_enable, set_cm55_nid_enable: 5;
-        }
-        impl AppCpussApCtl {
-            const ADDRESS: u64 = 0x441C1000;
-        }
-
-        let [cm33, cm55] = &*self.cores else {
-            unreachable!("PSOC Edge E84 devices have two cores");
-        };
-        let [cm33_ap, cm55_ap] = [cm33, cm55].map(|core| {
-            core.memory_ap()
-                .expect("PSOC Edge core must have a memory AP")
-        });
-
-        if core_ap == &cm55_ap {
-            // In order to debug the CM55, we may need to first power it up using the CM33..
-            let mut cm33_ap = interface.memory_interface(&cm33_ap)?;
+        if core_ap == &self.cm55_ap {
+            // In order to debug the CM55, we may need to first power it up using the CM33.
+            let mut cm33_ap = interface.memory_interface(&self.cm33_ap)?;
 
             // Check if the CM55 is enabled.
             let ctl = MxCm55Ctl(cm33_ap.read_word_32(MxCm55Ctl::ADDRESS)?);
             if ctl.cm55_wait() {
                 return Err(ArmError::CoreDisabled);
-            }
-
-            // Ensure the CM55 is powered up while the CM33 is powered (by the debug port)
-            let pd_sense: u32 = cm33_ap.read_word_32(AppCpuPdSense::ADDRESS)?;
-            if (pd_sense & (1 << SYSCPU_PD)) == 0 {
-                cm33_ap.write_word_32(AppCpuPdSense::ADDRESS, pd_sense | (1 << SYSCPU_PD))?;
             }
 
             // Enable the CM55 AP.
@@ -103,5 +96,24 @@ impl ArmDebugSequence for PsocEdge {
         }
 
         DefaultArmSequence(()).on_attach(interface, core_ap, core_type)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PsocEdge;
+    use crate::config::Registry;
+
+    #[test]
+    fn validate_psoc_edge_targets() {
+        let registry = Registry::from_builtin_families();
+        let family = registry
+            .families()
+            .iter()
+            .find(|family| family.name == "psoc_e84")
+            .unwrap();
+        for chip in family.variants() {
+            _ = PsocEdge::create(chip);
+        }
     }
 }
